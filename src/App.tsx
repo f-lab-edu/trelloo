7de87fbf1a531cb304359797dcc1c5d6b2cb159d
Gayun00 <gayunlee11@gmail.com>
2023-03-10 01:19:45 +0900

    Feat: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import styles from "./index.module.css";
+import "./styles/global.module.css";
+import "./styles/reset.module.css";
+import Board from "./components/Board";
 
 function App() {
-  return <div className={styles.app}></div>;
+  return (
+    <div>
+      <Board />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Board/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CardList from "../CardList";
+import styles from "./index.module.css";
+
+const Board = () => {
+  return (
+    <div>
+      Board
+      <CardList />
+    </div>
+  );
+};
+
+export default Board;
