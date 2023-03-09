7c7dbf13289cefa93a5c1d8c59f5fc05e015f536
Gayun00 <gayunlee11@gmail.com>
2023-03-10 01:19:45 +0900

    Feat: add components

---
+++ src/components/CardList/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "../Card";
+import styles from "./index.module.css";
+
+const CardList = () => {
+  return (
+    <div>
+      CardList
+      <Card />
+    </div>
+  );
+};
+
+export default CardList;
