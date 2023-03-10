614b04b12a85b507226940ae159d302249971bae
Gayun00 <gayunlee11@gmail.com>
2023-03-10 23:37:50 +0900

    Chore: use absolute import path

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,7 +1,7 @@
-import Board from "../components/Board";
+import Board from "@components/Board";
 import * as S from "./style";
 
-const BoardPage = (): JSX.Element => {
+const BoardPage = () => {
   return (
     <S.Container>
       <Board boardName="test board name" />
