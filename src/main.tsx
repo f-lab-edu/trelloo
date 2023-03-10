f0419939226e148b294dc3cf55bab48ad6c78dd3
Gayun00 <gayunlee11@gmail.com>
2023-03-10 21:58:30 +0900

    Chore: add styled-components setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./styles/theme";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </Provider>
-    ,
   </React.StrictMode>
 );
