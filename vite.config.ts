25885f5372e79a1e6dc664cd1ce40a765f00569d
Gayun00 <gayunlee11@gmail.com>
2023-03-10 23:37:50 +0900

    Chore: use absolute import path

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
         find: "@components",
         replacement: resolve(__dirname, "src/components"),
       },
+      {
+        find: "@utils",
+        replacement: resolve(__dirname, "src/utils"),
+      },
     ],
   },
 
