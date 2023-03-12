d9a4da557d72fdf02eeab7de99e43d05d4ccd042
gygy <67543454+Gayun00@users.noreply.github.com>
2023-03-12 23:12:44 +0900

    CHore: add clickoutside, drag and drop library (#11)
    * CHore: add clickoutside, drag and drop library
    
    * Refactor: add clickoutside to modal component
    
    - refactor useClickOutside
    
    * Modify: add theme variables
    
    * Correct: move useClickoutside folder

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,9 @@
+import { useRef } from "react";
 import { useSelector } from "react-redux";
 import { createPortal } from "react-dom";
 import { MODAL_TYPE } from "@utils/constants";
+import useOutsideClick from "@utils/useClickOutside";
+import useModal from "@utils/hooks/useModal";
 import { ModalState } from "@/store/slices/modal";
 import CardDetail from "@components/CardDetail";
 import CardEdit from "@components/CardEdit";
@@ -11,11 +14,13 @@ interface ModalComponent {
 }
 
 const Modal = () => {
+  const { closeModal } = useModal();
+  const clickOutsideRef = useRef<HTMLDivElement>(null);
   const { type, options, props } = useSelector(
     (state: { modal: ModalState }) => state.modal
   );
   const isModalOpened = type !== MODAL_TYPE.NONE ? true : false;
-
+  useOutsideClick(clickOutsideRef, () => closeModal());
   const modalComponent: ModalComponent = {
     [MODAL_TYPE.CARD_EDIT]: <CardEdit />,
     [MODAL_TYPE.CARD_DETAIL]: <CardDetail />,
@@ -24,9 +29,11 @@ const Modal = () => {
   return (
     <ModalPortal>
       {isModalOpened && (
-        <S.Container>
-          {modalComponent[type] && modalComponent[type]}
-        </S.Container>
+        <S.DimmedBackground>
+          <S.Container ref={clickOutsideRef}>
+            {modalComponent[type] && modalComponent[type]}
+          </S.Container>
+        </S.DimmedBackground>
       )}
     </ModalPortal>
   );
--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
+export const DimmedBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.color.dimmedBackground};
+`;
+
 export const Container = styled.div`
   // temporal style
   position: fixed;
   width: 300px;
   height: 300px;
-  background: beige;
+  background-color: white;
   transform: translate(25vw, 25vh);
 `;
