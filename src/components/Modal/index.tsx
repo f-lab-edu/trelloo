da69b895c09b4bf941f823f5239d8506fb75b42a
Gayun00 <gayunlee11@gmail.com>
2023-03-12 23:54:56 +0900

    Refactor: use code splitting to render modal content
    - give props to modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,37 +1,41 @@
-import { useRef } from "react";
+import React, { useRef, Suspense } from "react";
 import { useSelector } from "react-redux";
 import { createPortal } from "react-dom";
 import { MODAL_TYPE } from "@utils/constants";
-import useOutsideClick from "@utils/useClickOutside";
+import useOutsideClick from "@utils/hooks/useClickOutside";
 import useModal from "@utils/hooks/useModal";
 import { ModalState } from "@/store/slices/modal";
-import CardDetail from "@components/CardDetail";
-import CardEdit from "@components/CardEdit";
 import * as S from "./style";
 
 interface ModalComponent {
-  [key: string]: JSX.Element;
+  [key: string]: any;
 }
 
 const Modal = () => {
   const { closeModal } = useModal();
   const clickOutsideRef = useRef<HTMLDivElement>(null);
-  const { type, options, props } = useSelector(
+  const { type, props } = useSelector(
     (state: { modal: ModalState }) => state.modal
   );
   const isModalOpened = type !== MODAL_TYPE.NONE ? true : false;
   useOutsideClick(clickOutsideRef, () => closeModal());
   const modalComponent: ModalComponent = {
-    [MODAL_TYPE.CARD_EDIT]: <CardEdit />,
-    [MODAL_TYPE.CARD_DETAIL]: <CardDetail />,
+    [MODAL_TYPE.CARD_EDIT]: React.lazy(() => import("@components/CardEdit")),
+    [MODAL_TYPE.CARD_DETAIL]: React.lazy(
+      () => import("@components/CardDetail")
+    ),
   };
+
+  const CurrentComponent = modalComponent[type];
 
   return (
     <ModalPortal>
       {isModalOpened && (
         <S.DimmedBackground>
           <S.Container ref={clickOutsideRef}>
-            {modalComponent[type] && modalComponent[type]}
+            <Suspense fallback={<div>is loading...</div>}>
+              {modalComponent[type] && <CurrentComponent {...props} />}
+            </Suspense>
           </S.Container>
         </S.DimmedBackground>
       )}
