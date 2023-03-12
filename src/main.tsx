ca20a9a8fff928f1e31df57f5a0fa353402e2952
gygy <67543454+Gayun00@users.noreply.github.com>
2023-03-12 22:01:05 +0900

    Chore: react-query, msw (#10)
    * Chore: react-query, msw
    
    * Add: reqct-query wrapping function
    
    - use axios instead of fetch
    - add wrapping function of useQuery, useMutation
    - need to refactor to handle other options
    
    * Refactor: add options to handleUseQuery, handleUseMutation
    
    * Refactor: add type TRes to handle mutation response
    
    * Correct: rename type replacing axios to request
    
    - remove test code

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "./store";
 import { ThemeProvider } from "styled-components";
+import { worker } from "./mocks/browser";
 import App from "./App";
 import { theme } from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
