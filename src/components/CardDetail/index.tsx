e1e046259c3edd3f91bc7804a024dd620b3d829d
Gayun00 <gayunlee11@gmail.com>
2023-03-12 23:54:56 +0900

    Refactor: use code splitting to render modal content
    - give props to modal component

--- src/components/CardDetail/index.tsx
+++ src/components/CardDetail/index.tsx
@@ -1,12 +1,12 @@
 import useModal from "@utils/hooks/useModal";
 import * as S from "./style";
 
-const CardDetail = () => {
+const CardDetail = ({ title }: { title: string }) => {
   const { closeModal } = useModal();
 
   return (
     <S.Container>
-      CardDetail
+      {title}
       <S.CloseButton onClick={closeModal}>X</S.CloseButton>
     </S.Container>
   );
