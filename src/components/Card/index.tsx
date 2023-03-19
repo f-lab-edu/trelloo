a78f2253eff27c14a30c3797a02ecf6191ed8426
Gayun00 <gayunlee11@gmail.com>
2023-03-20 01:10:16 +0900

    Add: Drawer stories

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,13 @@
-// import { MODAL_TYPE } from "@/constants";
-// import useModal from "@/hooks/useModal";
+import { MODAL_TYPE } from "@/constants";
+import useModal from "@/hooks/useModal";
 import * as S from "./style";
 
 const Card = ({ text }: { text: string }) => {
-  // const { openModal } = useModal();
+  const { openModal } = useModal();
 
-  // const onClickCard = () => {
-  //   openModal({ type: MODAL_TYPE.CARD_DETAIL, props: { title: "cardDetail" } });
-  // };
+  const onClickCard = () => {
+    openModal({ type: MODAL_TYPE.CARD_DETAIL, props: { title: "cardDetail" } });
+  };
 
   return <S.Container>Card</S.Container>;
 };
