7e834cdd0172d7ac16982aa9af785abe71f715eb
Gayun00 <gayunlee11@gmail.com>
2023-03-19 21:47:11 +0900

    Add: Header component

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,9 +1,11 @@
+import Header from "@components/Header";
 import Board from "@components/Board";
 import * as S from "./style";
 
 const BoardPage = () => {
   return (
     <S.Container>
+      <Header />
       <Board boardName="test board name" />
     </S.Container>
   );
