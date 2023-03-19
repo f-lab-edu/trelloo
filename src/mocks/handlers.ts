dd4d39ae4d8337a049d48e9ee2a34f4202731819
Gayun00 <gayunlee11@gmail.com>
2023-03-19 19:03:24 +0900

    Refactor: add interfaces of cardLists api request response

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,11 +2,11 @@ import { rest } from "msw";
 import { cardLists } from "./data/cards";
 
 export const handlers = [
-  rest.get("/card/list", (req, res, ctx) => {
+  rest.get("/cards/lists", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(cardLists));
   }),
 
-  rest.post("/card/list", (req, res, ctx) => {
+  rest.post("/cards", (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.json({
