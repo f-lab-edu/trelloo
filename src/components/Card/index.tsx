6839c4ae5b36dd14a196a18cedac71808361e19b
Gayun00 <gayunlee11@gmail.com>
2023-03-21 00:26:52 +0900

    Add: basic stories
    - add stories to each folders
    - remove examples

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,14 +2,18 @@ import { MODAL_TYPE } from "@/constants";
 import useModal from "@/hooks/useModal";
 import * as S from "./style";
 
-const Card = ({ text }: { text: string }) => {
+interface Props {
+  text: string;
+}
+
+const Card = ({ text }: Props) => {
   const { openModal } = useModal();
 
   const onClickCard = () => {
     openModal({ type: MODAL_TYPE.CARD_DETAIL, props: { title: "cardDetail" } });
   };
 
-  return <S.Container>Card</S.Container>;
+  return <S.Container>{text && text}</S.Container>;
 };
 
 export default Card;
