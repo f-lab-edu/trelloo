b733597198e89b29fe8b4c01c418d82050ae493f
Gayun00 <gayunlee11@gmail.com>
2023-03-21 00:23:08 +0900

    Chore: edit storybook settings
    - add vite plugin
    - add @utils path resolution

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,13 +3,14 @@ module.exports = {
   viteFinal: async (config, { configType }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
+      "@utils": path.resolve(__dirname, "../src/utils"),
       "@components": path.resolve(__dirname, "../src/components"),
       "@": path.resolve(__dirname, "../src"),
     };
 
     return config;
   },
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
