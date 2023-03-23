39b57314167467d720593b58568805198a23d0de
Gayun00 <gayunlee11@gmail.com>
2023-03-23 23:39:50 +0900

    Refactor: use common interface ModalState
    - seperate ModalState as a common interface
    - add onRequestClose when it supports single modal

--- src/components/Modals/Modal/index.tsx
+++ src/components/Modals/Modal/index.tsx
@@ -1,17 +1,18 @@
+import { ModalState } from "@/interfaces/modal";
 import React, { Suspense } from "react";
 import ReactModal from "react-modal";
-import { ModalState } from "../ModalsProvider";
 import * as S from "./style";
 
-interface Props<P> {
-  Component: React.ComponentType<P>;
+interface Props<P> extends ModalState<P> {
   onClose: (state: ModalState<P>) => void;
-  props: P;
 }
 
-const Modal = <P extends {}>({ Component, onClose, props }: Props<P>) => {
+const Modal = <P extends {}>({ component: Component, onClose, props, isMultiple }: Props<P>) => {
+  const handleClose = () => {
+    if (!isMultiple) onClose({ component: Component, props });
+  };
   return (
-    <ReactModal isOpen={true} style={S.ModalStyle}>
+    <ReactModal isOpen={true} style={S.ModalStyle} onRequestClose={handleClose}>
       <button onClick={() => onClose({ component: Component, props })}>x</button>
       <Suspense fallback={<div>is loading...</div>}>
         <Component {...props} />
