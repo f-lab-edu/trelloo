249a30f01674991ab89f41c7a62a3bb0a6116f7b
Gayun00 <gayunlee11@gmail.com>
2023-03-25 13:08:46 +0900

    Correct: format code

--- src/components/Modals/Modal/index.tsx
+++ src/components/Modals/Modal/index.tsx
@@ -11,6 +11,7 @@ const Modal = <P extends {}>({ component: Component, onClose, props, options }: 
   const handleClose = () => {
     onClose({ component: Component, props });
   };
+
   return (
     <ReactModal isOpen={true} style={S.ModalStyle(options)} onRequestClose={handleClose}>
       <button onClick={() => onClose({ component: Component, props, options })}>x</button>
