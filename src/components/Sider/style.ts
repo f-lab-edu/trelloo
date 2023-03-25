5b69b36d7abd5eeee21df5fb41ce0210ee135ab4
Gayun00 <gayunlee11@gmail.com>
2023-03-26 01:40:12 +0900

    Modify: Sider
    - add icons, menu text

---
+++ src/components/Sider/style.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const SiderButton = styled.div`
+  padding: 14px 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProfileContainer = styled.div`
+  padding-bottom: 14px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  border-bottom: 1px solid lightgray;
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ProfileImg = styled.div`
+  width: 36px;
+  height: 36px;
+  background-color: aliceblue;
+`;
+
+export const ProfileText = styled.span`
+  margin-left: 10px;
+`;
