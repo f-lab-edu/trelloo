a9201060a498df7bb394843c2507b89bfebeffa9
Gayun00 <gayunlee11@gmail.com>
2023-03-25 10:29:40 +0900

    Refactor: add modal template story
    - correct: remove unused code

---
+++ src/templateStories/ModalTemplate/index.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "@/store";
+import ModalsProvider from "@components/Modals/ModalsProvider";
+import Modals from "@components/Modals/Modals";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Template = ({ children }: Props) => {
+  return (
+    <Provider store={store}>
+      <ModalsProvider>
+        {children}
+        <Modals />
+      </ModalsProvider>
+    </Provider>
+  );
+};
+
+export const ModalTemplate = Template.bind({});
