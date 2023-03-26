6c1ee22966072e297e250beedf2744dba8b3c65c
Gayun00 <gayunlee11@gmail.com>
2023-03-26 21:09:29 +0900

    Add: Header, Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,18 @@
 import React from "react";
-import { Button } from "antd";
+import {
+  StarOutlined,
+  UsergroupAddOutlined,
+  ThunderboltOutlined,
+  HistoryOutlined,
+  TableOutlined,
+  DownOutlined,
+  RocketOutlined,
+  FilterOutlined,
+  UserAddOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
+import * as S from "./style";
+import Button from "@components/Buttons/Button";
 
 interface Props {
   showDrawer: any;
@@ -7,13 +20,66 @@ interface Props {
 
 function Menu({ showDrawer }: Props) {
   return (
-    <div>
-      Menu
-      <Button type="primary" onClick={showDrawer}>
-        Open
-      </Button>
-    </div>
+    <S.Container>
+      <S.ButtonsWrapper>
+        {buttonList.map((button) => (
+          <Button icon={button.icon} buttonColor="gray">
+            {button.text}
+          </Button>
+        ))}
+      </S.ButtonsWrapper>
+      <S.OtherButtonsWrapper>
+        {buttonList2.map((button) => (
+          <Button icon={button.icon} buttonColor="gray">
+            {button.text}
+          </Button>
+        ))}
+        <Button icon={<EllipsisOutlined />} onClick={showDrawer} buttonColor="gray" />
+      </S.OtherButtonsWrapper>
+    </S.Container>
   );
 }
 
 export default Menu;
+
+const buttonList = [
+  {
+    text: "",
+    icon: <StarOutlined />,
+  },
+  {
+    text: "Workspace available",
+    icon: <UsergroupAddOutlined />,
+  },
+  {
+    text: "Board",
+    icon: <TableOutlined />,
+  },
+  {
+    text: "",
+    icon: <DownOutlined />,
+  },
+];
+
+const buttonList2 = [
+  {
+    text: "Repeats",
+    icon: <HistoryOutlined />,
+  },
+  {
+    text: "Power-ups",
+    icon: <RocketOutlined />,
+  },
+  {
+    text: "Automation",
+    icon: <ThunderboltOutlined />,
+  },
+  {
+    text: "Filter",
+    icon: <FilterOutlined />,
+  },
+  {
+    text: "Share",
+    icon: <UserAddOutlined />,
+  },
+];
