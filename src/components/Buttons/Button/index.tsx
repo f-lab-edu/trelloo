21a3fb8744752519d6db9f842f419d1225904f8f
Gayun00 <gayunlee11@gmail.com>
2023-03-26 21:08:27 +0900

    Refactor: Button
    - modify button wrapping with component

---
+++ src/components/Buttons/Button/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button as B } from "antd";
+import * as S from "./style";
+
+interface Props {
+  icon?: React.ReactNode;
+  isIconBehindText?: boolean;
+  buttonColor?: string;
+  children?: React.ReactNode;
+  onClick?: any;
+}
+function Button({ icon, isIconBehindText = false, buttonColor, onClick, children }: Props) {
+  return (
+    <S.Container onClick={onClick} buttonColor={buttonColor}>
+      <B icon={!isIconBehindText && icon} type="text">
+        {children}
+        {isIconBehindText && icon}
+      </B>
+    </S.Container>
+  );
+}
+
+export default Button;
