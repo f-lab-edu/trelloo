13b86cdf7cc890adb1772d8b8d83c7acef3eab93
Gayun00 <gayunlee11@gmail.com>
2023-03-26 21:09:29 +0900

    Add: Header, Menu

---
+++ src/components/Menu/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  background-color: "transparent";
+`;
+
+export const ButtonsWrapper = styled.span`
+  display: flex;
+`;
+export const OtherButtonsWrapper = styled.span`
+  display: flex;
+`;
+
+export const Button = {
+  backgroundColor: "transparent",
+};
