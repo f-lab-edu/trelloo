798c3a92d567a199e4232b8e985156883e1bb4f3
Gayun00 <gayunlee11@gmail.com>
2023-03-26 15:31:33 +0900

    Modify: Card, CardList using antd

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,10 +2,11 @@ export const theme = {
   color: {
     dimmedBackground: "#000000a3",
     backgroundGray: "#F3F5F7", // modal background
-    pointGray: "#EAECF0", // list backgoround, button
+    cardListGray: "#EAECF0",
     hoverPointGray: "#E1E4E9",
     textPointGray: "#5E6C83",
     textDefaultGray: "#3C3E3F",
     siderBackground: "#E2E8ED",
+    cardBoxShadow: "#091e4240",
   },
 };
