e2bc6f57d7028d314081b633520309a5e91aa933
Gayun00 <gayunlee11@gmail.com>
2023-03-26 21:09:29 +0900

    Add: Header, Menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 import { Layout } from "antd";
+import { AppstoreOutlined, BellOutlined, QuestionCircleOutlined, DownOutlined, UserOutlined } from "@ant-design/icons";
+import Button from "@components/Buttons/Button";
+import * as S from "./style";
 
 const { Header: H } = Layout;
 
 function Header() {
   return (
-    <H style={{ position: "sticky", top: 0, zIndex: 1, width: "100%", backgroundColor: "white" }}>
-      <div>header</div>
+    <H style={S.Header}>
+      <S.Container>
+        <S.Nav>
+          <Button icon={<AppstoreOutlined />} />
+          <Button icon={<AppstoreOutlined />}>Trello</Button>
+          <Button icon={<DownOutlined />} isIconBehindText={true}>
+            Workspaces
+          </Button>
+          <Button icon={<DownOutlined />} isIconBehindText={true}>
+            Recent
+          </Button>
+          <Button icon={<DownOutlined />} isIconBehindText={true}>
+            Starred
+          </Button>
+          <Button icon={<DownOutlined />} isIconBehindText={true}>
+            Templates
+          </Button>
+          <Button>Create</Button>
+        </S.Nav>
+        <S.ButtonsWrapper>
+          <Button icon={<BellOutlined />} />
+          <Button icon={<QuestionCircleOutlined />} />
+          <Button icon={<UserOutlined />} />
+        </S.ButtonsWrapper>
+      </S.Container>
     </H>
   );
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
+export const Header: React.CSSProperties = {
+  position: "sticky",
+  top: 0,
+  padding: "6px 4px",
+  zIndex: 1,
+  width: "100%",
+  height: 44,
+  backgroundColor: "white",
+};
+
 export const Container = styled.header`
-  background-color: antiquewhite;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  height: 40px;
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
 `;
