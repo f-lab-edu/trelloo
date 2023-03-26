9014600a90d2357fd78ec3c26fecf9fe16c45c3b
Gayun00 <gayunlee11@gmail.com>
2023-03-27 00:18:54 +0900

    Refactor: isMultiple prop of Modal
    - use hasOverlay prop insteak of isMultiple
    - change order of provider hierarchy
    - add top, left prop to handle modal position

--- src/interfaces/modal.ts
+++ src/interfaces/modal.ts
@@ -2,6 +2,8 @@ export interface ModalState<TProps = any> {
   component: React.ComponentType<TProps>;
   props: TProps;
   options?: {
-    isMultiple?: boolean;
+    hasOverlay?: boolean;
+    top?: number;
+    left?: number;
   };
 }
