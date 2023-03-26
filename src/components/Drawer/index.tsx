96fb4cf8f6326569167df282600f4adcd9b50da2
Gayun00 <gayunlee11@gmail.com>
2023-03-26 21:07:57 +0900

    Modify: Drawer using antd

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,15 +1,68 @@
 import React from "react";
+import { Menu } from "antd";
+import type { MenuProps } from "antd";
 import { Drawer as D } from "antd";
+import {
+  TableOutlined,
+  SettingOutlined,
+  UserOutlined,
+  CalendarOutlined,
+  AppstoreOutlined,
+  UngroupOutlined,
+} from "@ant-design/icons";
+import * as S from "./style";
 
 interface Props {
   open: any;
   onClose: any;
 }
 
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: "group",
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+const items2: MenuProps["items"] = [
+  getItem("About this board", "About this board", <AppstoreOutlined />),
+  getItem("Change background", "Change background", <UserOutlined />),
+  getItem("Custom fields", "Custom fields", <SettingOutlined />),
+  getItem("Stickers", "Stickers", <SettingOutlined />),
+  getItem("More", "More", <SettingOutlined />),
+  getItem(
+    "Stickers",
+    "grp",
+    null,
+    [getItem("Table", "13", <TableOutlined />), getItem("Calendar", "14", <CalendarOutlined />)],
+    "group",
+  ),
+  getItem("Your boards", "grp", null, [getItem("my board", "15", <UngroupOutlined />)], "group"),
+];
+
 function Drawer({ open, onClose }: Props) {
   return (
-    <D title="Basic Drawer" placement="right" closable={false} onClose={onClose} open={open} getContainer={false}>
-      <p>Some contents...</p>
+    <D
+      title="Menu"
+      placement="right"
+      closable={false}
+      onClose={onClose}
+      open={open}
+      getContainer={false}
+      bodyStyle={S.BodyStyle}
+    >
+      <Menu mode="inline" defaultSelectedKeys={["1"]} defaultOpenKeys={["sub1"]} items={items2} style={S.Menu} />
     </D>
   );
 }
