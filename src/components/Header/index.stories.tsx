0146632432f499dc74a89576a3803a879cfb4004
Gayun00 <gayunlee11@gmail.com>
2023-03-26 21:09:29 +0900

    Add: Header, Menu

--- src/components/Header/index.stories.tsx
+++ src/components/Header/index.stories.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "@components/Header";
+import { StoryProvider } from "@/index.stories";
 
 export default {
   title: "Components/Header",
@@ -11,7 +12,11 @@ export default {
 };
 
 const Template = (args: { isBlue?: boolean }) => {
-  return <Header />;
+  return (
+    <StoryProvider>
+      <Header />
+    </StoryProvider>
+  );
 };
 
 export const Primary = Template.bind({});
