a57482d58e35705a96d1afd7ab00ecb018bc6dc1
Gayun00 <gayunlee11@gmail.com>
2023-03-26 15:31:33 +0900

    Modify: Card, CardList using antd

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,11 +1,21 @@
+import { Card as C } from "antd";
+import { DashOutlined } from "@ant-design/icons";
 import Card from "@components/Card";
 import * as S from "./style";
 
 const CardList = () => {
   return (
     <S.Container>
-      CardList
-      <Card text="card" />
+      <C
+        title="Card list"
+        bordered={false}
+        style={{ width: 300 }}
+        extra={<DashOutlined />}
+        headStyle={S.Header}
+        bodyStyle={S.Body}
+      >
+        <Card text="card" />
+      </C>
     </S.Container>
   );
 };
