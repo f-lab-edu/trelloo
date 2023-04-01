b56f76b811fce15af064cda20871ab2c7584a0de
Gayun00 <gayunlee11@gmail.com>
2023-04-01 15:34:28 +0900

    Refactor: add card request
    - rename Modals, Buttons folder to lower case
    - move add card queries to Board component
    - rename AddCard component to CardComposer
    - rename handleClickAddCard to toggleCardInput
    - edit handleAddCard props type
    - give boardName props to Menu, not Board
    - edit queryKey of card list request
    - seperate AddCardRequest interface to interface file

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,18 +1,26 @@
-import { useGetCardLists } from "@/queries/cardList";
+import { useAddCardMutation, useGetCardLists } from "@/queries/cardList";
+import { AddCardRequest } from "@/queries/cardList/interface";
 import CardList from "@components/CardList";
 import * as S from "./style";
 
-interface Props {
-  boardName: string;
-}
-
-const Board = ({ boardName }: Props) => {
+const Board = () => {
   const { data: cardLists } = useGetCardLists();
+
+  const { refetch } = useGetCardLists();
+  const { mutate } = useAddCardMutation();
+
+  const handleAddCard = ({ text, listId }: AddCardRequest) => {
+    mutate({
+      text,
+      listId,
+    });
+    refetch();
+  };
 
   return (
     <S.Container>
       {cardLists?.map((cardList) => (
-        <CardList key={cardList.id} data={cardList} />
+        <CardList key={cardList.id} data={cardList} handleAddCard={handleAddCard} />
       ))}
     </S.Container>
   );
