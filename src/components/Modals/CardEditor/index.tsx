64b548e71850195dd56de59c5f9a2dbe02681b85
Gayun00 <gayunlee11@gmail.com>
2023-04-02 13:59:04 +0900

    Add: CardEditor UI
    - add input,buttons

--- src/components/Modals/CardEditor/index.tsx
+++ src/components/Modals/CardEditor/index.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import * as S from "./style";
+import { Input, theme } from "antd";
+import Button from "@components/Button";
+
+const { TextArea } = Input;
 
 export interface Props {
   text: string;
@@ -8,7 +12,20 @@ export interface Props {
 const CardEditor = ({ text }: Props) => {
   return (
     <S.Container>
-      <S.Input>{text}</S.Input>
+      <S.InputWrapper>
+        <TextArea defaultValue={text} placeholder="Controlled autosize" autoSize={{ minRows: 3, maxRows: 5 }} />
+        <Button buttonColor={"blue"}>Save</Button>
+      </S.InputWrapper>
+      <S.MenuButtonsWrapper>
+        <Button buttonColor={"gray"}>Open card</Button>
+        <Button buttonColor={"gray"}>Edit labels</Button>
+        <Button buttonColor={"gray"}>Change members</Button>
+        <Button buttonColor={"gray"}>Change cover</Button>
+        <Button buttonColor={"gray"}>Move</Button>
+        <Button buttonColor={"gray"}>Copy</Button>
+        <Button buttonColor={"gray"}>Edit dates</Button>
+        <Button buttonColor={"gray"}>Archive</Button>
+      </S.MenuButtonsWrapper>
     </S.Container>
   );
 };
