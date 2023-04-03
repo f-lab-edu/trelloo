8c44752438a7cf6906fbffcae4aadbfcf8711941
Gayun00 <gayunlee11@gmail.com>
2023-04-03 14:52:34 +0900

    Add: edit card request
    - add indexed db, msw
    - add mutation

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,5 +1,5 @@
-import { useAddCardMutation, useAddListMutation, useGetCardLists } from "@/queries/cardList";
-import { AddCardRequest, AddListRequest } from "@/queries/cardList/interface";
+import { useAddCardMutation, useAddListMutation, useEditCardMutation, useGetCardLists } from "@/queries/cardList";
+import { AddCardRequest, AddListRequest, EditCardRequest } from "@/queries/cardList/interface";
 import CardList from "@components/CardList";
 import CardListComposer from "@components/CardListComposer";
 import * as S from "./style";
@@ -10,11 +10,20 @@ const Board = () => {
   const { refetch } = useGetCardLists();
   const { mutate: addCardMutate } = useAddCardMutation();
   const { mutate: addListMutate } = useAddListMutation();
+  const { mutate: editCardMutate } = useEditCardMutation();
 
   const handleAddCard = ({ text, listId }: AddCardRequest) => {
     addCardMutate({
       text,
       listId,
+    });
+    refetch();
+  };
+
+  const handleEditCard = ({ text, id }: EditCardRequest) => {
+    editCardMutate({
+      text,
+      id,
     });
     refetch();
   };
@@ -29,7 +38,7 @@ const Board = () => {
   return (
     <S.Container>
       {cardLists?.map((cardList) => (
-        <CardList key={cardList.id} data={cardList} onAddCardClick={handleAddCard} />
+        <CardList key={cardList.id} data={cardList} onAddCardClick={handleAddCard} onEditCard={handleEditCard} />
       ))}
       <CardListComposer onClick={handleAddList} />
     </S.Container>
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,19 +1,22 @@
+import React, { useState } from "react";
 import { Card as AntdCard } from "antd";
 import { EditOutlined } from "@ant-design/icons";
 import loadable from "@loadable/component";
-import React, { useState } from "react";
 import useModal from "@/hooks/useModal";
+import { EditCardRequest } from "@/queries/cardList/interface";
 const CardDetail = loadable(() => import("@components/modals/CardDetail"));
 const CardEditor = loadable(() => import("@components/modals/CardEditor"));
 import * as S from "./style";
 
 interface Props {
   data: {
+    id: string;
     text: string;
   };
+  onEditCard: (data: EditCardRequest) => void;
 }
 
-const Card = ({ data }: Props) => {
+const Card = ({ data, onEditCard }: Props) => {
   const { openModal } = useModal();
   const [isCardEditorOpened, setIsCardEditorOpened] = useState(false);
 
@@ -35,7 +38,7 @@ const Card = ({ data }: Props) => {
         <p>{data.text}</p>
         <EditOutlined className="edit_button" onClick={handleOpenCardEditor} />
       </AntdCard>
-      {isCardEditorOpened && <CardEditor text={data.text} onClick={handleOpenCardEditor} />}
+      {isCardEditorOpened && <CardEditor data={data} onClick={handleOpenCardEditor} onEditCard={onEditCard} />}
     </S.Container>
   );
 };
