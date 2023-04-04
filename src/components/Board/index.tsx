1fe8c10f514d9105942cbc798d8343e28622a493
Gayun00 <gayunlee11@gmail.com>
2023-04-04 11:15:53 +0900

    Refactor: seperate Card, CardList handler providers
    - wrap components with each providers that is used

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -2,20 +2,20 @@ import { useGetCardLists } from "@/queries/cardList";
 
 import CardList from "@components/CardList";
 import CardListComposer from "@components/CardListComposer";
+import { CardHandlerProvider, CardListHandlerProvider } from "./Provider";
 import * as S from "./style";
-import Provider from "./Provider";
 
 const Board = () => {
   const { data: cardLists } = useGetCardLists();
 
   return (
     <S.Container>
-      <Provider>
+      <CardListHandlerProvider>
         {cardLists?.map((cardList) => (
           <CardList key={cardList.id} data={cardList} />
         ))}
         <CardListComposer />
-      </Provider>
+      </CardListHandlerProvider>
     </S.Container>
   );
 };
