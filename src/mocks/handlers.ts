c0f93e908c55406b4b6d007618ba7fb401d805bb
Gayun00 <gayunlee11@gmail.com>
2023-04-04 20:46:55 +0900

    Refactor: api endpoint, interfaces
    - reuse interfaces in mock handlers
    - edit responses of api requests

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,12 @@
+import {
+  AddCardRequest,
+  AddListRequest,
+  DeleteCardRequest,
+  DeleteListRequest,
+  EditCardRequest,
+  EditListRequest,
+  ResponseMessage,
+} from "@/queries/cardList/interface";
 import { rest } from "msw";
 import { v4 as uuidv4 } from "uuid";
 import {
@@ -10,111 +19,88 @@ import {
   getAllCardListsWithCards,
 } from "./dbfunctions";
 
-interface AddCardRequestBody {
-  text: string;
-  listId: string;
-}
-
-interface EditCardRequestBody {
-  id: string;
-  text: string;
-}
-
-interface DeleteCardRequestBody {
-  id: string;
-}
-
-interface AddListRequestBody {
-  title: string;
-}
-
-interface EditListRequestBody {
-  id: string;
-  title: string;
-}
-
-interface DeleteListRequestBody {
-  id: string;
-}
-
-interface DefaultResponseBody {
-  message: string;
-}
-
 export const handlers = [
-  rest.get("/cards/lists", (req, res, ctx) => {
+  rest.get("/cards", (req, res, ctx) => {
     return getAllCardListsWithCards().then((data) => {
       return res(ctx.status(200), ctx.json(data));
     });
   }),
 
-  rest.post<string, DefaultResponseBody>("/cards", (req, res, ctx) => {
-    const { text, listId } = JSON.parse(req.body) as AddCardRequestBody;
-    return addCard({ listId, text, id: uuidv4(), createdAt: Date.now() }).then(() => {
+  rest.post<string, ResponseMessage>("/cards", (req, res, ctx) => {
+    const { text, listId } = JSON.parse(req.body) as AddCardRequest;
+    const id = uuidv4();
+
+    return addCard({ listId, text, id, createdAt: Date.now() }).then(() => {
       return res(
-        ctx.status(200),
+        ctx.status(201),
         ctx.json({
-          message: "card created",
+          message: "Card created",
+          id,
+          text,
         }),
       );
     });
   }),
 
-  rest.post<string, DefaultResponseBody>("/list", (req, res, ctx) => {
-    const { title } = JSON.parse(req.body) as AddListRequestBody;
-    return addCardList({ title, id: uuidv4(), createdAt: Date.now() }).then(() => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          message: "list created",
-        }),
-      );
-    });
-  }),
-
-  rest.put<string, DefaultResponseBody>("/list", (req, res, ctx) => {
-    const { id, title } = JSON.parse(req.body) as EditListRequestBody;
-    return editCardList({ id, title }).then(() => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          message: "list updated",
-        }),
-      );
-    });
-  }),
-
-  rest.delete<string, DefaultResponseBody>("/list", (req, res, ctx) => {
-    const { id } = JSON.parse(req.body) as DeleteListRequestBody;
-    return deleteCardList({ id }).then(() => {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          message: "list deleted",
-        }),
-      );
-    });
-  }),
-
-  rest.put<string, DefaultResponseBody>("/cards", (req, res, ctx) => {
-    const { id, text } = JSON.parse(req.body) as EditCardRequestBody;
+  rest.put<string, ResponseMessage>("/cards", (req, res, ctx) => {
+    const { id, text } = JSON.parse(req.body) as EditCardRequest;
     return editCard({ id, text }).then(() => {
       return res(
         ctx.status(200),
         ctx.json({
-          message: "card updated",
+          message: "Card updated",
         }),
       );
     });
   }),
 
-  rest.delete<string, DefaultResponseBody>("/card", (req, res, ctx) => {
-    const { id } = JSON.parse(req.body) as DeleteCardRequestBody;
+  rest.delete<string, ResponseMessage>("/cards", (req, res, ctx) => {
+    const { id } = JSON.parse(req.body) as DeleteCardRequest;
     return deleteCard({ id }).then(() => {
       return res(
         ctx.status(200),
         ctx.json({
-          message: "card is deleted",
+          message: "Card deleted",
+        }),
+      );
+    });
+  }),
+
+  rest.post<string, ResponseMessage>("/lists", (req, res, ctx) => {
+    const { title } = JSON.parse(req.body) as AddListRequest;
+    const id = uuidv4();
+
+    return addCardList({ title, id, createdAt: Date.now() }).then(() => {
+      return res(
+        ctx.status(201),
+        ctx.json({
+          message: "List created",
+          title,
+          id,
+        }),
+      );
+    });
+  }),
+
+  rest.put<string, ResponseMessage>("/lists", (req, res, ctx) => {
+    const { id, title } = JSON.parse(req.body) as EditListRequest;
+    return editCardList({ id, title }).then(() => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          message: "List updated",
+        }),
+      );
+    });
+  }),
+
+  rest.delete<string, ResponseMessage>("/lists", (req, res, ctx) => {
+    const { id } = JSON.parse(req.body) as DeleteListRequest;
+    return deleteCardList({ id }).then(() => {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          message: "List deleted",
         }),
       );
     });
