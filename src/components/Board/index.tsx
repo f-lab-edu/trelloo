e0fd6461657b7b7f943869eba9e2827c8c6c2465
Gayun00 <gayunlee11@gmail.com>
2023-04-06 23:49:52 +0900

    Refactor: use event handler to give props to CardList

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,5 +1,6 @@
 import { useCardsQuery } from "@/queries/cards";
 import { useAddListMutation, useDeleteListMutation, useEditListMutation } from "@/queries/cards";
+import { AddListRequest, DeleteListRequest, EditListRequest } from "@/queries/cards/interface";
 import CardList from "@components/CardList";
 import CardListComposer from "@components/CardListComposer";
 import * as S from "./style";
@@ -10,12 +11,24 @@ const Board = () => {
   const { mutate: deleteListMutate } = useDeleteListMutation();
   const { mutate: editListMutate } = useEditListMutation();
 
+  const onEditList = (params: EditListRequest) => {
+    editListMutate(params);
+  };
+
+  const onDeleteList = (params: DeleteListRequest) => {
+    deleteListMutate(params);
+  };
+
+  const onAddList = (params: AddListRequest) => {
+    addListMutate(params);
+  };
+
   return (
     <S.Container>
       {cardLists?.map((cardList) => (
-        <CardList key={cardList.id} data={cardList} onEditList={editListMutate} onDeleteList={deleteListMutate} />
+        <CardList key={cardList.id} data={cardList} onEditList={onEditList} onDeleteList={onDeleteList} />
       ))}
-      <CardListComposer onAddList={addListMutate} />
+      <CardListComposer onAddList={onAddList} />
     </S.Container>
   );
 };
