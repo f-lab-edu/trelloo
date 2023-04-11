05584826b4e97e26ceaae4e0534d1f4a99dffdb3
Gayun00 <gayunlee11@gmail.com>
2023-04-11 20:48:50 +0900

    Add: login page

---
+++ src/pages/LoginPage/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding-top: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+
+  .login_form {
+    margin-top: 70px;
+    width: 500px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 30px;
+  font-weight: 800;
+  text-align: center;
+`;
