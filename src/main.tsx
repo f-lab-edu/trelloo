d8c671da7db508417174fbb542b5411f3601400a
Gayun00 <gayunlee11@gmail.com>
2023-04-15 23:54:00 +0900

    Chore: add react-toastify setting

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import ReactModal from "react-modal";
+import { ToastContainer } from "react-toastify";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "./store";
@@ -11,6 +12,7 @@ import ModalsProvider from "@components/modals/ModalsProvider";
 import App from "./App";
 import { theme } from "./styles/theme";
 import { GlobalStyle } from "./styles/GlobalStyle";
+import "react-toastify/dist/ReactToastify.css";
 
 if (process.env.NODE_ENV === "development") {
   worker.start();
@@ -20,17 +22,18 @@ const queryClient = new QueryClient();
 ReactModal.setAppElement("#root");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools initialIsOpen={false} />
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <ModalsProvider>
-          <GlobalStyle />
-          <App />
-        </ModalsProvider>
-      </ThemeProvider>
-    </Provider>
-  </QueryClientProvider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <ModalsProvider>
+            <GlobalStyle />
+            <App />
+            <ToastContainer position="bottom-left" theme="dark" autoClose={1500} hideProgressBar={true} limit={1} />
+          </ModalsProvider>
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
