3755af03333aa738fed0dc6799dd7db746b8e5cc
Gayun00 <gayunlee11@gmail.com>
2023-04-15 13:59:11 +0900

    Add: login route
    - add storage key constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BoardPage from "./pages/BoardPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<BoardPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
