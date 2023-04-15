6f98f265c2ed5d00fa78ffd3078ecf5383a30667
Gayun00 <gayunlee11@gmail.com>
2023-04-15 14:18:43 +0900

    Add: empty login page story template

---
+++ src/pages/LoginPage/index.stories.tsx
@@ -0,0 +1,14 @@
+import LoginPage from ".";
+
+export default {
+  title: "Pages/LoginPage",
+  component: LoginPage,
+  argTypes: {},
+};
+
+const Template = () => {
+  // TODO: add story
+  return <></>;
+};
+
+export const Primary = Template.bind({});
