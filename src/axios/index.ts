7b48b88f8c2043cab20b26a2577027dce52045b6
Gayun00 <gayunlee11@gmail.com>
2023-04-16 15:14:33 +0900

    Refacotr: axios intercepters
    - add setupInterceptors
    - use function expressions for callback functions

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,5 +1,22 @@
-import axiosInstance from "./instance";
-import "./requestIntercepters";
-import "./responseIntercepters";
+import axios, { AxiosInstance } from "axios";
+import { handleInterceptConfig, handleRquestInterceptError } from "./requestIntercepters";
+import { handleInterceptResponse, handleResponseInterceptError } from "./responseIntercepters";
+
+const axiosInstance = axios.create({
+  headers: {
+    common: {
+      "Content-Type": "application/json",
+    },
+  },
+});
+
+const setupInterceptors = (instance: AxiosInstance): AxiosInstance => {
+  instance.interceptors.request.use(handleInterceptConfig, handleRquestInterceptError);
+  instance.interceptors.response.use(handleInterceptResponse, handleResponseInterceptError);
+
+  return instance;
+};
+
+setupInterceptors(axiosInstance);
 
 export default axiosInstance;
