b7dfe2c77bf1cc47503afa4bdcf199cedb17fcd7
Gayun00 <gayunlee11@gmail.com>
2023-04-16 22:36:31 +0900

    Refactor: seperate Board component wrapped with ErrorBoundary

--- src/pages/BoardPage/index.tsx
+++ src/pages/BoardPage/index.tsx
@@ -1,14 +1,10 @@
-import React, { useState, Suspense } from "react";
+import React, { useState } from "react";
 import { Layout } from "antd";
-import { QueryErrorResetBoundary } from "@tanstack/react-query";
-import { ErrorBoundary } from "react-error-boundary";
 import Sider from "@components/Sider";
 import Drawer from "@components/Drawer";
 import Header from "@components/Header";
 import Menu from "@components/Menu";
 import Board from "@components/Board";
-import BoardSkeleton from "@components/skeletons/BoardSkeleton";
-import EmptyBoard from "@components/EmptyBoard";
 import * as S from "./style";
 
 const { Content } = Layout;
@@ -31,18 +27,7 @@ const BoardPage: React.FC = () => {
         <Sider />
         <Content style={S.Content}>
           <Menu showDrawer={showDrawer} boardName={"boardName"} />
-          <QueryErrorResetBoundary>
-            {({ reset }) => (
-              <ErrorBoundary
-                onReset={reset}
-                fallbackRender={({ resetErrorBoundary }) => <EmptyBoard onQueryErrorReset={resetErrorBoundary} />}
-              >
-                <Suspense fallback={<BoardSkeleton />}>
-                  <Board />
-                </Suspense>
-              </ErrorBoundary>
-            )}
-          </QueryErrorResetBoundary>
+          <Board />
           <Drawer open={open} onClose={onClose} />
         </Content>
       </Layout>
