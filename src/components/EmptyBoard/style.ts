cbf161c866aa925b1bd01a91942d01b00b9930c7
Gayun00 <gayunlee11@gmail.com>
2023-04-16 20:09:46 +0900

    Add: QueryErrorResetBoundary
    - add try again button in EmptyBoard to reset query error

--- src/components/EmptyBoard/style.ts
+++ src/components/EmptyBoard/style.ts
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding-top: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   width: 100vw;
   height: 100vh;
-  display: flex;
-  justify-content: center;
 `;
