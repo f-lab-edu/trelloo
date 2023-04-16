316f52ef07cc4bcd02a75b2bb00d5743aa685e71
Gayun00 <gayunlee11@gmail.com>
2023-04-17 01:40:49 +0900

    Add: CardDetailModal UI

--- src/components/Modals/CardDetailModal/index.tsx
+++ src/components/Modals/CardDetailModal/index.tsx
@@ -1,11 +1,45 @@
+import { Space, Input } from "antd";
+import Button from "@components/Button";
+import CardDetailModalButtons from "@components/CardDetailModalButtons";
 import * as S from "./style";
 
 export interface Props {
   title: string;
+  onClose: () => void;
 }
 
-const CardDetailModal = ({ title }: Props) => {
-  return <S.Container>{title}</S.Container>;
+const CardDetailModal = ({ title, onClose }: Props) => {
+  return (
+    <S.Container>
+      <S.CloseButton onClick={onClose} />
+      <Space size="large" direction="vertical">
+        <S.Section>
+          <S.Title>{title}</S.Title>
+          <Button appearance={{ type: "gray" }}>Watch</Button>
+        </S.Section>
+
+        <S.MainWrapper>
+          <S.Main>
+            <Space size="large" direction="vertical">
+              <S.Section>
+                <S.Title>Description</S.Title>
+                <S.DescriptionInput></S.DescriptionInput>
+              </S.Section>
+
+              <S.Section>
+                <S.Title>Activity</S.Title>
+                <Input></Input>
+              </S.Section>
+            </Space>
+          </S.Main>
+
+          <S.Sider>
+            <CardDetailModalButtons />
+          </S.Sider>
+        </S.MainWrapper>
+      </Space>
+    </S.Container>
+  );
 };
 
 export default CardDetailModal;
