463c4ba0b61f163d059fb5b7e2b5508a46b99cc9
Gayun00 <gayunlee11@gmail.com>
2023-04-16 20:51:38 +0900

    Add: disable add card button on loading

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as S from "./style";
 
 interface Props {
+  isLoading?: boolean;
   Icon?: React.ReactNode;
   isIconBehindText?: boolean;
   type?: "button" | "submit" | "reset";
@@ -13,9 +14,9 @@ interface Props {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-function Button({ Icon, isIconBehindText = false, type = "button", appearance, onClick, children }: Props) {
+function Button({ Icon, isIconBehindText = false, type = "button", appearance, onClick, children, isLoading }: Props) {
   return (
-    <S.Container type={type} style={appearance?.style} onClick={onClick} appearance={appearance}>
+    <S.Container type={type} style={appearance?.style} onClick={onClick} appearance={appearance} disabled={isLoading}>
       <S.IconWrapper>{!isIconBehindText && !!Icon && Icon}</S.IconWrapper>
       {children}
       <S.IconBehindWrapper>{isIconBehindText && Icon}</S.IconBehindWrapper>
