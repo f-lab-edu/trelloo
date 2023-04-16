25f42ecc5ddd6586013213e187a9afd41fd85608
Gayun00 <gayunlee11@gmail.com>
2023-04-16 11:42:13 +0900

    Refactor: handle queryParams using axios
    - delete handling searchParams
    - rename params to data, queryParams to params: axios suppport handling searchParams, using a params parameter. rename previous variables to avoid being duplicated.

--- src/interfaces/httpRequest.ts
+++ src/interfaces/httpRequest.ts
@@ -1,32 +1,32 @@
-export interface RequestParams<TQueryParams = Params, TParams = {}> {
+export interface RequestParams<TParams = data, TData = {}> {
   path: string;
   method?: RequestMethod;
+  data?: TData;
   params?: TParams;
-  queryParams?: TQueryParams;
   isMock?: boolean;
 }
 
-export type Params = Record<string, string>;
+export type data = Record<string, string>;
 type RequestMethod = "get" | "post" | "put" | "delete";
 
-export interface RequestOptions<TParams> {
-  onSuccess?: (data: TParams) => void;
+export interface RequestOptions<TData> {
+  onSuccess?: (data: TData) => void;
   onError?: (error: Error) => void;
 }
 
-export interface HandleUseQueryParams<TQueryParams = Params, TParams = object> {
+export interface HandleUseQueryParams<TParams = data, TData = object> {
   key: string;
   path: string;
-  queryParams?: TQueryParams;
-  options?: RequestOptions<TParams>;
+  params?: TParams;
+  options?: RequestOptions<TData>;
 }
 
-export interface HandleUseMutationParams<TQueryParams = Params, TParams = object, TRes = object> {
+export interface HandleUseMutationParams<TParams = data, TData = object, TRes = object> {
   key?: string;
   path: string;
   method: RequestMethod;
-  queryParams?: TQueryParams;
   params?: TParams;
+  data?: TData;
   options?: RequestOptions<TRes>;
   res?: TRes;
 }
