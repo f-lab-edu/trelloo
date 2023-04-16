5caad64d508ed62fea6310f24e87514716d1fcc2
Gayun00 <gayunlee11@gmail.com>
2023-04-16 13:33:52 +0900

    Refactor: login mutation
    - remove unused code

--- src/queries/auth/index.ts
+++ src/queries/auth/index.ts
@@ -1,19 +1,9 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { request } from "@utils/httpRequest";
 import { LoginRequest, LoginResponse } from "./interface";
 
-const authKeys = {
-  all: ["auth"] as const,
-};
-
 export const useLoginMutation = () => {
-  const queryClient = useQueryClient();
-  return useMutation(
-    (params: LoginRequest) => {
-      return request.post<LoginResponse>({ path: "/login", isMock: true, params });
-    },
-    {
-      onSuccess: () => queryClient.invalidateQueries(authKeys.all),
-    },
-  );
+  return useMutation((data: LoginRequest) => {
+    return request.post<LoginResponse>({ path: "/login", isMock: true, data });
+  });
 };
