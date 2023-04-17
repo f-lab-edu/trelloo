8f1276ab07fdee963388434d825424d662a13622
Gayun00 <gayunlee11@gmail.com>
2023-04-18 00:50:01 +0900

    Refactor: move CardEditor from Modal to components

---
+++ src/components/CardEditor/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { Input } from "antd";
+import { ICard } from "@/interfaces/cards";
+import { DeleteCardRequest, EditCardRequest } from "@/queries/cards/interface";
+import Button from "@components/Button";
+import CardEditorButtons from "@components/CardEditorButtons";
+import * as S from "./style";
+
+const { TextArea } = Input;
+
+export interface Props {
+  data: ICard;
+  onCardEditorClose: (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
+  setCardEditorOpened: (state: boolean) => void;
+  onEditCard: (params: EditCardRequest) => void;
+  onDeleteCard: (params: DeleteCardRequest) => void;
+}
+
+const CardEditor = ({ data, onCardEditorClose, setCardEditorOpened, onEditCard, onDeleteCard }: Props) => {
+  const [inputValue, setInputValue] = useState(data.description);
+
+  const handleClickSave = (params: { id: string; description: string }) => {
+    setCardEditorOpened(false);
+    onEditCard(params);
+  };
+
+  return (
+    <>
+      <S.Overlay onClick={onCardEditorClose} />
+      <S.Container>
+        <S.InputWrapper>
+          <TextArea
+            defaultValue={data.description}
+            onChange={(e) => setInputValue(e.target.value)}
+            autoSize={{ minRows: 3, maxRows: 5 }}
+          />
+          <S.SaveButtonWrapper>
+            <Button
+              appearance={{ type: "blue" }}
+              onClick={() => handleClickSave({ id: data.id, description: inputValue })}
+            >
+              Save
+            </Button>
+          </S.SaveButtonWrapper>
+        </S.InputWrapper>
+        <S.MenuButtonsWrapper>
+          <CardEditorButtons onDeleteCard={() => onDeleteCard({ id: data.id })} />
+        </S.MenuButtonsWrapper>
+      </S.Container>
+    </>
+  );
+};
+
+export default CardEditor;
