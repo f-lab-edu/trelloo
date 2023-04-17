9c4593316feccad72fc1d2fe23271252bd2288dc
Gayun00 <gayunlee11@gmail.com>
2023-04-18 00:50:01 +0900

    Refactor: move CardEditor from Modal to components

---
+++ src/components/CardEditor/index.stories.tsx
@@ -0,0 +1,25 @@
+import CardEditor from "@components/CardEditor";
+
+export default {
+  title: "Components/Cards",
+  component: CardEditor,
+};
+
+const Template = () => {
+  const data = { id: "id", description: "description" };
+  const onCardEditorClose = () => {};
+  const setCardEditorOpened = () => {};
+  const onEditCard = () => {};
+  const onDeleteCard = () => {};
+
+  return (
+    <CardEditor
+      data={data}
+      onCardEditorClose={onCardEditorClose}
+      setCardEditorOpened={setCardEditorOpened}
+      onEditCard={onEditCard}
+      onDeleteCard={onDeleteCard}
+    />
+  );
+};
+export const InviteToWorkspaceModal = Template.bind({});
