416a1898392bf356539547fc38d2b11b3aed5a15
Gayun00 <gayunlee11@gmail.com>
2023-04-21 01:03:18 +0900

    Refactor: use rhf to handle CardComposer

--- src/components/CardComposer/style.ts
+++ src/components/CardComposer/style.ts
@@ -18,7 +18,7 @@ export const ButtonWrapper = styled.div`
   align-items: center;
 `;
 
-export const CardInputContainer = styled.div`
+export const CardInputContainer = styled.form`
   margin-top: 10px;
 `;
 
