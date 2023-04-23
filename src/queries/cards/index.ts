37cb196881ce6fcc0edbf3c0b980622b70b4249d
Gayun00 <gayunlee11@gmail.com>
2023-04-23 22:15:35 +0900

    Refactor: add optimistic update on drag on drop

--- src/queries/cards/index.ts
+++ src/queries/cards/index.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { request } from "@/utils/httpRequest";
 import type * as I from "./interface";
+import { ICardList } from "@/interfaces/cards";
 
 const cardListsKeys = {
   all: ["cardLists"] as const,
@@ -105,7 +106,7 @@ export const useEditListMutation = () => {
 export const useEditCardPositionMutation = () => {
   const queryClient = useQueryClient();
   return useMutation(
-    ({ cardId, listId, index }: I.EditCardPositionParam & EditCardPositionRequest) => {
+    ({ cardId, listId, index }: I.EditCardPositionParam & I.EditCardPositionRequest) => {
       return request.put<I.ResponseMessage>({
         path: `/cards/${cardId}/move`,
         isMock: true,
@@ -113,8 +114,35 @@ export const useEditCardPositionMutation = () => {
       });
     },
     {
-      onSuccess: async () => {
-        await queryClient.invalidateQueries(cardListsKeys.all);
+      onSuccess: () => queryClient.invalidateQueries(cardListsKeys.all),
+      onMutate: ({ cardId, listId, index }) => {
+        const previousData = queryClient.getQueryData<ICardList[]>(cardListsKeys.all);
+        if (!previousData) return;
+
+        const previousList = previousData.filter((list) => list.cards.some((card) => card.id === cardId));
+        const previousListId = previousList[0].id;
+        const card = previousList[0].cards.filter((card) => card.id === cardId)[0];
+
+        const updatedData = previousData.map((list) => {
+          if (list.id === listId) {
+            const newCards = Array.from(list.cards);
+            newCards.splice(index, 0, card);
+            return { ...list, cards: newCards };
+          } else if (list.id === previousListId) {
+            const newCards = Array.from(list.cards);
+            newCards.splice(card.index, 1);
+            return { ...list, cards: newCards };
+          } else {
+            return list;
+          }
+        });
+
+        queryClient.setQueryData(cardListsKeys.all, updatedData);
+
+        return () => queryClient.setQueryData(cardListsKeys.all, previousData);
+      },
+      onError: (error, variables, rollback) => {
+        rollback?.();
       },
     },
   );
