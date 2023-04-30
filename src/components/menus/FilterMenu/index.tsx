ff848944dad180c758a288bb6c6553e1934e6f83
Gayun00 <gayunlee11@gmail.com>
2023-04-30 09:56:33 +0900

    Modify: use search input

--- src/components/menus/FilterMenu/index.tsx
+++ src/components/menus/FilterMenu/index.tsx
@@ -1,101 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { Checkbox } from "antd";
-import type { CheckboxValueType } from "antd/es/checkbox/Group";
+import React from "react";
 import * as S from "./style";
-import { useNavigate } from "react-router-dom";
-
-interface CheckedState {
-  member: string[];
-  dueComplete: boolean;
-}
-
-const plainOptions = ["Apple", "Pear", "Orange"];
-
-const options = [
-  { label: "No members", value: "none" },
-  { label: "Card assigned to me", value: "user" },
-];
 
 function FilterMenu() {
-  const navigate = useNavigate();
-  const [checkedValues, setCheckedValues] = useState<CheckedState>({
-    member: [],
-    dueComplete: false,
-  });
-
-  const searchParams = new URLSearchParams(window.location.search);
-
-  const handleCheck = (key: string, values: CheckboxValueType[] | boolean) => {
-    const stringValues = typeof values === "boolean" ? String(values) : values.map(String);
-
-    setCheckedValues({
-      ...checkedValues,
-      [key]: stringValues,
-    });
-  };
-
-  const mapKeyValues = (key: string, values: string[] | string) => {
-    if (typeof values === "string") {
-      return `${key}:${values}`;
-    }
-    if (values.length > 0) {
-      return values.map((value: string[] | string) => `${key}:${value}`).join(",");
-    }
-  };
-
-  const handleQueryString = () => {
-    const checkedValueLists = Object.entries(checkedValues);
-    const queryString = checkedValueLists
-      .reduce((acc, [key, values]) => {
-        return (acc += `${mapKeyValues(key, values)},`);
-      }, "")
-      .slice(0, -1);
-
-    return queryString;
-  };
-
-  useEffect(() => {
-    const queryString = handleQueryString();
-    searchParams.set("filter", queryString);
-    navigate(window.location.pathname + "?" + searchParams.toString());
-  }, [checkedValues]);
-
   return (
-    <S.Container>
-      <Checkbox.Group
-        options={plainOptions}
-        onChange={(e) => {
-          handleCheck("member", e);
-        }}
-      />
-      <br />
-      <br />
-      <p>Members</p>
-      <Checkbox.Group
-        options={options}
-        onChange={(e) => {
-          handleCheck("member", e);
-        }}
-      />
-      <br />
-      <br />
-      <Checkbox
-        onChange={(e) => {
-          handleCheck("dueComplete", e.target.checked);
-        }}
-        checked={Boolean(checkedValues.dueComplete)}
-      >
-        Marked as resolved
-      </Checkbox>
-      <Checkbox
-        onChange={(e) => {
-          handleCheck("dueComplete", !e.target.checked);
-        }}
-        checked={!checkedValues.dueComplete}
-      >
-        Marked as resolved
-      </Checkbox>
-    </S.Container>
+    <div>
+      <S.Title>Keyword</S.Title>
+      <S.Input placeholder="Enter a keyword..." />
+      <S.Description>Search cards, members, labels, and more.</S.Description>
+    </div>
   );
 }
 
