dff9651f17268215614091edd20a8f70d998ca43
Gayun00 <gayunlee11@gmail.com>
2023-04-30 12:13:14 +0900

    Add: Input common component

--- src/components/menus/FilterMenu/index.tsx
+++ src/components/menus/FilterMenu/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
+import Input from "@components/inputs/Input";
+import { useForm } from "react-hook-form";
 import * as S from "./style";
 
 function FilterMenu() {
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      filterMenu: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = (data: any) => {
+    // TODO: add onSubmit
+  };
+
   return (
     <div>
       <S.Title>Keyword</S.Title>
-      <S.Input placeholder="Enter a keyword..." />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input control={control} placeHolder="Enter a keyword..." name="filterMenu" />
+      </form>
       <S.Description>Search cards, members, labels, and more.</S.Description>
     </div>
   );
