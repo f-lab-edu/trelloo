9666f92f69b46cc23799cf6abb264caa22233ade
Gayun00 <gayunlee11@gmail.com>
2023-05-01 08:57:35 +0900

    Add: Composer common component

---
+++ src/components/forms/Composer/style.ts
@@ -0,0 +1,36 @@
+import { CloseOutlined } from "@ant-design/icons";
+import { theme } from "@/styles/theme";
+import styled from "styled-components";
+
+export const Container = styled.form``;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  border-radius: ${({ theme }) => theme.borderRadius.card};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.hoverPointGray};
+  }
+`;
+
+export const InputWrapper = styled.div`
+  background-color: ${({ theme }) => theme.color.cardListGray};
+`;
+
+export const SubmitButtonWrapper = styled.div`
+  margin-top: 8px;
+  display: flex;
+`;
+
+export const CloseButton = styled(CloseOutlined).attrs(() => ({
+  style: {
+    marginLeft: 5,
+    padding: 0,
+    display: "flex",
+    alignItems: "center",
+    fontSize: 20,
+    color: theme.color.textPointGray,
+  },
+}))``;
