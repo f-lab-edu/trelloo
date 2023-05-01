c9c95f92d1f5056fd625375f05dec30b195e052b
Gayun00 <gayunlee11@gmail.com>
2023-05-01 09:35:41 +0900

    Refactor: CardListComposer using Composer
    - correct composer removing unused styles

--- src/components/CardComposer/style.ts
+++ src/components/CardComposer/style.ts
@@ -1,5 +1,4 @@
 import { Card as AntdCard } from "antd";
-import { CloseOutlined } from "@ant-design/icons";
 import { theme } from "@/styles/theme";
 import styled from "styled-components";
 
@@ -11,32 +10,3 @@ export const Card = styled(AntdCard).attrs(() => ({
     borderRadius: theme.borderRadius.card,
   },
 }))``;
-
-export const ButtonWrapper = styled.div`
-  margin-top: 5px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-export const CardInputContainer = styled.form`
-  margin-top: 10px;
-`;
-
-export const AddCardButtonContainer = styled.div`
-  padding: 5px 0;
-  display: flex;
-  justify-content: space-between;
-`;
-
-export const AddCardButtonWrapper = styled.span`
-  display: flex;
-  align-items: center;
-`;
-
-export const CloseButton = styled(CloseOutlined)<{ theme: typeof theme }>`
-  margin-left: 5px;
-  padding: 0;
-  font-size: 20px;
-  color: ${({ theme }) => theme.color.textPointGray};
-`;
--- src/components/CardListComposer/style.ts
+++ src/components/CardListComposer/style.ts
@@ -1,37 +1,10 @@
-import { CloseOutlined } from "@ant-design/icons";
-import { theme } from "@/styles/theme";
 import styled from "styled-components";
 
 export const Container = styled.div`
   margin-left: 10px;
-  width: 272px;
-`;
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  height: 40px;
-  border-radius: ${({ theme }) => theme.borderRadius.card};
-  background-color: ${({ theme }) => theme.color.hoverPointGray};
-`;
-
-export const InputWrapper = styled.div`
-  padding: 5px;
+  padding: 8px;
+  min-width: 272px;
   background-color: ${({ theme }) => theme.color.cardListGray};
+  border-radius: ${({ theme }) => theme.borderRadius.card};
+  height: 100%;
 `;
-
-export const SubmitButtonWrapper = styled.div`
-  margin-top: 3px;
-  display: flex;
-`;
-
-export const CloseButton = styled(CloseOutlined).attrs(() => ({
-  style: {
-    marginLeft: 5,
-    padding: 0,
-    display: "flex",
-    alignItems: "center",
-    fontSize: 20,
-    color: theme.color.textPointGray,
-  },
-}))``;
