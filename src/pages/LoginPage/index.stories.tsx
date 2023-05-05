6d0583d78f1eeb07f723dc400732ee3c49fa19bf
Gayun00 <gayunlee11@gmail.com>
2023-05-05 11:23:06 +0900

    Chore: add react-router addon in login page

--- src/pages/LoginPage/index.stories.tsx
+++ src/pages/LoginPage/index.stories.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { withRouter } from "storybook-addon-react-router-v6";
 import LoginPage from ".";
 
 export default {
   title: "Pages/LoginPage",
   component: LoginPage,
+  decorators: [withRouter],
+  parameters: {
+    reactRouter: {
+      routePath: "/board",
+    },
+  },
 };
 
 const Template = () => {
