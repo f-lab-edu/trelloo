1bc318ba7c88c3cdcbfbaf2ae567863bff04ba37
gygy <67543454+Gayun00@users.noreply.github.com>
2023-05-16 10:24:13 +0900

    Refactor: Modal props type inference (#81)
    * Chore: Fix ts.config.json
    
    * Refactor: modal prop typs to be inferred automatically
    
    * Refactor: seperate OpenModal, OpenModalState
    
    * Refactor: closeModal, use component props as Component in Modals
    
    * Refactor: rename modalContents to modals
    
    - chore: move modalContents to modals/components/index
    
    * Refactor: remove destructuring in useModal

--- src/interfaces/modal.ts
+++ src/interfaces/modal.ts
@@ -1,11 +1,8 @@
-import { LoadableComponent } from "@loadable/component";
-
-type ModalContentsKey = "cardDetailModal" | "inviteToWorkspaceModal";
-export type ModalContents<TProps> = Record<ModalContentsKey, LoadableComponent<TProps>>;
+import { ComponentType } from "react";
 
 export interface ModalState<TProps = any> {
-  component: ModalContentsKey;
-  props?: TProps;
+  component: ComponentType<TProps>;
+  props?: Omit<TProps, "onClose">;
   index: number;
   options?: {
     hasOverlay?: boolean;
@@ -14,5 +11,7 @@ export interface ModalState<TProps = any> {
   };
 }
 
+export type OpenModal<TProps> = Omit<ModalState<TProps>, "index">;
+
 export type OpenModalState = Omit<ModalState, "index">;
-export type CloseModalState = Pick<ModalState, "component" | "index">;
+export type CloseModalState = Pick<ModalState, "index">;
