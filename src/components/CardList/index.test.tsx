64629740d175407ccf3d11b5ba4cb533675bbd14
Gayun00 <gayunlee11@gmail.com>
2023-05-25 23:50:36 +0900

    Refactor: use query mocking in CardList test

--- src/components/CardList/index.test.tsx
+++ src/components/CardList/index.test.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 import CardList from "@components/CardList";
-import { customRender, fireEvent } from "@utils/testUtils";
+import { customRender, fireEvent, renderHook, screen, waitFor } from "@utils/testUtils";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
-import { describe, vi } from "vitest";
+import { describe, vi, expect } from "vitest";
+import { ICardList } from "@/interfaces/cards";
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+import { mockedCardList, mockedCardLists } from "@components/Board/mockData";
 
 describe("CardListComposer 테스트", () => {
   const handleEditList = vi.fn();
   const handleDeleteList = vi.fn();
   const handleDragEnd = vi.fn();
 
-  const cardList = {
-    id: "",
-    title: "list title",
-    cards: [
-      {
-        id: "",
-        description: "card",
-        index: 0,
+  function useCardsQuery() {
+    return useQuery({
+      queryKey: ["cards"],
+      queryFn: () => {
+        return mockedCardLists;
       },
-    ],
-  };
+    });
+  }
 
-  const setup = () => {
+  const setup = (cardList: ICardList | undefined) => {
     const { getByText, getByPlaceholderText } = customRender(
-      <DragDropContext onDragEnd={handleDragEnd}>
+      <DragDropContext onDragEnd={handleDragEnd} key={cardList?.id}>
         <Droppable droppableId="cardlistId">
           {(provided) => (
             <div ref={provided.innerRef} {...provided.droppableProps}>
-              <CardList data={cardList} onEditList={handleEditList} onDeleteList={handleDeleteList} />
+              {cardList && <CardList data={cardList} onEditList={handleEditList} onDeleteList={handleDeleteList} />}
               {provided.placeholder}
             </div>
           )}
@@ -35,25 +35,46 @@ describe("CardListComposer 테스트", () => {
       </DragDropContext>,
     );
 
-    const card = getByText("card");
-    const list = getByText("list title");
-    const cardInputToggleButton = getByText("Add a card");
-    return { getByText, getByPlaceholderText, card, list, cardInputToggleButton };
+    return { getByText, getByPlaceholderText };
   };
 
   it("카드와 카드 목록, Add a card 버튼 렌더링", () => {
-    const { card, list, cardInputToggleButton } = setup();
+    const { getByText } = setup(mockedCardList);
+    screen.debug();
+    const cardInputToggleButton = getByText("Add a card");
+    const card = getByText("card1");
+    const list = getByText("list1");
+
     expect(card).toBeInTheDocument();
     expect(list).toBeInTheDocument();
     expect(cardInputToggleButton).toBeInTheDocument();
   });
 
   it("Add a card 버튼 클릭 시 카드 인풋창과 Add card 버튼 렌더링", () => {
-    const { getByText, getByPlaceholderText, cardInputToggleButton } = setup();
+    const { getByText, getByPlaceholderText } = setup(mockedCardList);
+    screen.debug();
+    const cardInputToggleButton = getByText("Add a card");
+
     fireEvent.click(cardInputToggleButton);
     const addCardButton = getByText("Add card");
     const cardInput = getByPlaceholderText("Enter a title for this card...");
     expect(addCardButton).toBeInTheDocument();
     expect(cardInput).toBeInTheDocument();
+  });
+
+  it("인풋창에 텍스트 입력 후 제출 버튼 클릭 시 새 카드 생성", async () => {
+    const queryClient = new QueryClient();
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    );
+
+    const { result } = renderHook(() => useCardsQuery(), { wrapper });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    const cardLists = result.current.data;
+    expect(cardLists).toEqual(mockedCardLists);
+    const { getByText } = setup(cardLists?.data[0]);
+    const listTitle = getByText("listTitle1");
+    expect(listTitle).toBeInTheDocument();
   });
 });
