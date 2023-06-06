7e07673827126cab2cc396134ae03b569948afad
gygy <67543454+Gayun00@users.noreply.github.com>
2023-06-06 20:02:23 +0900

    Add: 컴포넌트 테스트 추가 (#86)
    * Chore: add component test setting
    
    - add react-testing-library and vitest
    
    * Add: test
    
    * Add: CardList, Composer test
    
    * Refactor: test
    
    * Refactor: add mock query, mutation hook to test render Board component
    
    - refactor useCardsQuery: add interface, wrap response with code, data as an object
    
    * Refactor: use query mocking in CardList test
    
    * Refactor: CardComposer, Input test
    
    * Refactor: remove state manipulating in CardComposer test
    
    * Refactor: remove unused code
    
    * Refactor: use createMockedQuery utils to mock useQuery hook
    
    * Add: snapshot test
    
    * Correct: remove unused async
    
    * Add: Composer test
    
    - add snapshot test
    - add test for function call on click event
    
    * Refactor: Composer test
    
    - give input related props from cardList to Board
    - add CardListComposer test
    - fix click event handler moving into waitFor statement
    - add Composer test
    
    * Refactor: Board test
    
    - remove mutation result type assertion
    
    * Refactor: remove destructuring mutation result in Board, CardList
    
    * Chore: add react, react-dom

---
+++ src/components/Board/mockData.ts
@@ -0,0 +1,49 @@
+export const mockedCardLists = {
+  code: 1,
+  data: [
+    {
+      id: "list1",
+      title: "list1",
+      cards: [
+        {
+          id: "card1",
+          description: "card1",
+          index: 0,
+        },
+        {
+          id: "card2",
+          description: "card2",
+          index: 1,
+        },
+      ],
+    },
+    {
+      id: "list2",
+      title: "list2",
+      cards: [
+        {
+          id: "card2-1",
+          description: "card2-1",
+          index: 2,
+        },
+      ],
+    },
+  ],
+};
+
+export const mockedCardList = {
+  id: "list1",
+  title: "list1",
+  cards: [
+    {
+      id: "card1",
+      description: "card1",
+      index: 0,
+    },
+    {
+      id: "card2",
+      description: "card2",
+      index: 1,
+    },
+  ],
+};
