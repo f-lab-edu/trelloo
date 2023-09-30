f8893b707d9020c4b24b6bde7a13a4f6ac39a387
gygy <67543454+Gayun00@users.noreply.github.com>
2023-09-30 15:45:35 +0900

    Refactor usefunnel (#11)
    * Refactor: use state to handle funnels in change background instead of query parameters
    
    * Refactor: add useFunnel
    
    * Fix: type issue in useFunnel

--- src/components/drawerMenus/ChangeBackground/index.tsx
+++ src/components/drawerMenus/ChangeBackground/index.tsx
@@ -1,35 +1,48 @@
 import React from "react";
-import { DRAWER_MENU_SEARCH_PARAMS, SEARCH_PARAMS_KEY } from "@/constants";
 import ImageCard from "@components/cards/listCard";
 import Menu from "@components/menuList";
-import { useSearchParams } from "react-router-dom";
+import BackgroundPhotos from "../BackgroundPhotos";
+import BackgroundColors from "@components/BackgroundColors";
+import useFunnel from "@/hooks/useFunnel";
 
 function ChangeBackground() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const handleMenu = () => {
-    searchParams.set(SEARCH_PARAMS_KEY.MENU, DRAWER_MENU_SEARCH_PARAMS.PHOTOS);
-    setSearchParams(searchParams);
-  };
+  const { Funnel, Step, handleStep } = useFunnel<"changeBackground" | "photos" | "colors">("changeBackground");
 
   return (
     <div>
-      <Menu.Title hasGoBackButton>Change Background</Menu.Title>
+      <Funnel>
+        <Step name="changeBackground">
+          <Menu.Title hasGoBackButton>Change Background</Menu.Title>
 
-      <ImageCard.ImageList>
-        <div>
-          <ImageCard.Image
-            url="/public/images/backgrounds/background_1.png"
-            onClick={handleMenu}
-            name="background image"
-          />
-          <ImageCard.Title>Photos</ImageCard.Title>
-        </div>
+          <ImageCard.ImageList>
+            <div>
+              <ImageCard.Image
+                url="/public/images/backgrounds/background_1.png"
+                onClick={() => handleStep("photos")}
+                name="background image"
+              />
+              <ImageCard.Title>Photos</ImageCard.Title>
+            </div>
 
-        <div>
-          <ImageCard.Image url="/public/images/backgrounds/background_1.png" name="background image" />
-          <ImageCard.Title>Colors</ImageCard.Title>
-        </div>
-      </ImageCard.ImageList>
+            <div>
+              <ImageCard.Image
+                url="/public/images/backgrounds/background_1.png"
+                onClick={() => handleStep("colors")}
+                name="background image"
+              />
+              <ImageCard.Title>Colors</ImageCard.Title>
+            </div>
+          </ImageCard.ImageList>
+        </Step>
+
+        <Step name="photos">
+          <BackgroundPhotos />
+        </Step>
+
+        <Step name="colors">
+          <BackgroundColors />
+        </Step>
+      </Funnel>
     </div>
   );
 }
