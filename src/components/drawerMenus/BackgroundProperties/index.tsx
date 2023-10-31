0e24b2695cc8f079503ecd604385566051f18a2f
Gayun00 <gayunlee11@gmail.com>
2023-10-31 22:22:11 +0900

    Add: BackgroundProperties

---
+++ src/components/drawerMenus/BackgroundProperties/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./style";
+import Menu from "@components/menuList";
+
+function BackgroundProperties() {
+  return (
+    <S.Container>
+      <Menu.Title hasGoBackButton>Background Properties</Menu.Title>
+      <div></div>
+    </S.Container>
+  );
+}
+
+export default BackgroundProperties;
