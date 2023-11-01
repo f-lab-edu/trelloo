345f51f68f7524e9b2708d59f5307882c7fa8ecd
Gayun00 <gayunlee11@gmail.com>
2023-11-01 19:28:01 +0900

    Add: ImageButtonControl

---
+++ src/components/properties/controls/ImageButtonControl/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import * as S from "./style";
+
+function ImageButtonControl() {
+  return (
+    <S.Button variant="secondary">
+      <AiOutlinePlus />
+    </S.Button>
+  );
+}
+
+export default ImageButtonControl;
