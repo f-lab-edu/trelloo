f9e2b6cde09cf864b1bc7ad7491733e59b7efd00
Gayun00 <gayunlee11@gmail.com>
2023-11-01 19:28:34 +0900

    Add: ImageBoxControl

---
+++ src/components/properties/controls/ImageBoxControl/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  color: black;
+  border: 1px solid hsl(var(--input));
+  border-radius: 5px;
+  background-color: aliceblue;
+`;
