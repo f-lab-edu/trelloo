566e2dd84d7a01b6611afc403832b1b4522e01fa
Gayun00 <gayunlee11@gmail.com>
2023-11-01 19:51:48 +0900

    Add: ImageBoxControl, GridProperty

---
+++ src/components/properties/GridProperty/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 5px;
+  width: 100%;
+  height: 100%;
+`;
