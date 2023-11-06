a6ed1c232892350d1431cf68e0e7dce2e4578864
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor: handle login in login page"
    This reverts commit 492db98633d92230b003b712d3f3659e82eaedff.

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -2,33 +2,25 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
-import { Form } from "antd";
+import { Button, Form } from "antd";
 import { STORAGE_KEY } from "@/constants";
 import { useLoginMutation } from "@/queries/auth";
-import Button from "@components/Button";
 import * as S from "./style";
 
 const LoginPage: React.FC = () => {
   const navigate = useNavigate();
-  const { register, handleSubmit, watch } = useForm();
   const { mutateAsync: loginMutate } = useLoginMutation();
+  const { register, handleSubmit, watch } = useForm();
 
-  const handleSubmitClick = async () => {
-    const res = await handleLogin();
-    handleLoginComplete(res.accessToken);
-  };
-
-  const handleLogin = () => {
-    return loginMutate({
+  const handleSubmitClick = () => {
+    loginMutate({
       id: watch("id"),
       password: watch("password"),
+    }).then((res) => {
+      localStorage.setItem(STORAGE_KEY.TOKEN, res.accessToken);
+      navigate("/");
+      toast.success("Welcome!");
     });
-  };
-
-  const handleLoginComplete = (token: string) => {
-    localStorage.setItem(STORAGE_KEY.TOKEN, token);
-    navigate("/");
-    toast.success("Welcome!");
   };
 
   return (
@@ -42,7 +34,7 @@ const LoginPage: React.FC = () => {
         onFinish={handleSubmit(handleSubmitClick)}
         autoComplete="off"
       >
-        <Form.Item label="Id" name="id">
+        <Form.Item label="id" name="id">
           <S.Input {...register("id")} name="id" />
         </Form.Item>
 
@@ -51,7 +43,8 @@ const LoginPage: React.FC = () => {
         </Form.Item>
 
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button appearance={{ type: "blue" }} type="submit">
+          {/* TODO: replace to the Button component instead of antd */}
+          <Button type="primary" htmlType="submit" onClick={handleSubmitClick}>
             Submit
           </Button>
         </Form.Item>
