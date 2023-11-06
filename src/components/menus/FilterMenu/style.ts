087ed8ac10a25aa1f9580cb7ba1d2a3ad8601a86
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Modify: use search input"
    This reverts commit babe67097f9dc8f2bdebadfade79c1154d6a49ec.

--- src/components/menus/FilterMenu/style.ts
+++ src/components/menus/FilterMenu/style.ts
@@ -1,25 +1,5 @@
 import styled from "styled-components";
-import { Input as AntdInput } from "antd";
 
 export const Container = styled.div`
   width: 300px;
-`;
-
-export const Title = styled.h3`
-  font-size: 12px;
-  font-weight: 600;
-  line-height: 16px;
-  margin-top: 16px;
-  margin-bottom: 8px;
-`;
-
-export const Input = styled(AntdInput).attrs(() => ({}))`
-  width: 300px;
-`;
-
-export const Description = styled.p`
-  font-size: 11px;
-  line-height: 14px;
-  margin-top: 8px;
-  margin-bottom: 4px;
 `;
