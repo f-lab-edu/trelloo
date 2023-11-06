2fbbb6a3dc37daaec1021e2d14e48122ce9d1b17
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Fix: error after rebase dev"
    This reverts commit 8b40054939ebf121ed1704ec92e812644b10560b.

--- src/queries/cards/index.ts
+++ src/queries/cards/index.ts
@@ -107,12 +107,11 @@ export const useEditListMutation = () => {
 export const useEditCardPositionMutation = () => {
   const queryClient = useQueryClient();
   return useMutation<I.ResponseMessage, AxiosError, I.EditCardPositionRequest, I.EditCardMutationData>(
-    ({ cardId, listId, index }: I.EditCardPositionRequest) => {
+    ({ cardId, listId, index }: I.EditCardPositionParam & I.EditCardPositionRequest) => {
       return request.put<I.ResponseMessage>({
         path: `/cards/${cardId}/move`,
         isMock: true,
         params: { listId, index },
-        shouldAuthorize: true,
       });
     },
     {
