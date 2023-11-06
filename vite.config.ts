9ec8abf97333f3630b5a3a68ba0cbb9cdba83a09
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Chore: add absolute path setting"
    This reverts commit 404c5d6b4051b090bc8d6c6207c003f67d33f521.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [
-      { find: "@src", replacement: resolve(__dirname, "src") },
-      {
-        find: "@components",
-        replacement: resolve(__dirname, "src/components"),
-      },
-    ],
-  },
-
-  plugins: [react(), tsconfigPaths()],
-});
+  plugins: [react()],
+})
