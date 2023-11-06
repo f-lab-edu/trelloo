a0743d9cda48eb3e8cb37c90ff36bfb54fbd547e
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Chore: change storybook path resolve setting"
    This reverts commit f1f13e8c4a98f81a616ec31e0c6bca9793d904a4.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,18 @@
-const path = require("path");
 module.exports = {
-  viteFinal: async (config, { configType }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@components": path.resolve(__dirname, "../src/components"),
-      "@": path.resolve(__dirname, "../src"),
-    };
-
-    return config;
-  },
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-vite",
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  features: {
-    storyStoreV7: true,
-  },
-};
+  "features": {
+    "storyStoreV7": true
+  }
+}
