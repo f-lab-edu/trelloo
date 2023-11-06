cdccb79125ce4f2c272b29b868fd08b09da8d69d
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor: isMultiple prop of Modal"
    This reverts commit 91f0eb0290752440c127b3afb1f04a29a93551c4.

--- src/interfaces/modal.ts
+++ src/interfaces/modal.ts
@@ -2,8 +2,6 @@ export interface ModalState<TProps = any> {
   component: React.ComponentType<TProps>;
   props: TProps;
   options?: {
-    hasOverlay?: boolean;
-    top?: number;
-    left?: number;
+    isMultiple?: boolean;
   };
 }
