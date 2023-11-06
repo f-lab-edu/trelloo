6ede2c4fd814a64a4e7198fbc49cb20e145b17f9
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: CardEditor UI"
    This reverts commit 64b548e71850195dd56de59c5f9a2dbe02681b85.

--- src/components/Modals/CardEditor/index.tsx
+++ src/components/Modals/CardEditor/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import * as S from "./style";
-import { Input, theme } from "antd";
-import Button from "@components/Button";
-
-const { TextArea } = Input;
 
 export interface Props {
   text: string;
@@ -12,20 +8,7 @@ export interface Props {
 const CardEditor = ({ text }: Props) => {
   return (
     <S.Container>
-      <S.InputWrapper>
-        <TextArea defaultValue={text} placeholder="Controlled autosize" autoSize={{ minRows: 3, maxRows: 5 }} />
-        <Button buttonColor={"blue"}>Save</Button>
-      </S.InputWrapper>
-      <S.MenuButtonsWrapper>
-        <Button buttonColor={"gray"}>Open card</Button>
-        <Button buttonColor={"gray"}>Edit labels</Button>
-        <Button buttonColor={"gray"}>Change members</Button>
-        <Button buttonColor={"gray"}>Change cover</Button>
-        <Button buttonColor={"gray"}>Move</Button>
-        <Button buttonColor={"gray"}>Copy</Button>
-        <Button buttonColor={"gray"}>Edit dates</Button>
-        <Button buttonColor={"gray"}>Archive</Button>
-      </S.MenuButtonsWrapper>
+      <S.Input>{text}</S.Input>
     </S.Container>
   );
 };
