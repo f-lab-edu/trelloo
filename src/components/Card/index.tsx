0416b1003a0c583092949f60152881346b944cd3
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Correct: rename antd component"
    This reverts commit c0b80ddf8d748c342694d6ba2741ac455452cc4a.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,4 @@
-import { Card as AntdCard } from "antd";
+import { Card as C } from "antd";
 import loadable from "@loadable/component";
 import useModal from "@/hooks/useModal";
 const CardDetail = loadable(() => import("@components/Modals/CardDetail"));
@@ -23,9 +23,9 @@ const Card = ({ data }: Props) => {
 
   return (
     <S.Container>
-      <AntdCard style={S.Card} onClick={handleClick} bodyStyle={S.Body}>
+      <C style={S.Card} onClick={handleClick} bodyStyle={S.Body}>
         <p>{data.text}</p>
-      </AntdCard>
+      </C>
     </S.Container>
   );
 };
