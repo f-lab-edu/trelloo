f002c6375ac4f393f19708fc3cbce8e17a3fc49b
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor: use url constant"
    This reverts commit 1f112e572e27f63fe25b4e3ad9c979d4e6b90b8f.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,6 @@
-import { BASE_URL } from "./src/utils/constants";
 module.exports = {
   projectId: "5ouo1q",
   e2e: {
-    baseUrl: BASE_URL.LOCAL,
+    baseUrl: "http://127.0.0.1:5173/",
   },
 };
--- src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = {
-  LOCAL: "http://127.0.0.1:5173/",
-};
