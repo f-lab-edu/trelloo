c7abb5ad7055a5c12797eb9540d0247ef59ee5b8
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: add a new card"
    This reverts commit 0c266b60a0a079daef4c748f980f372e6f23d854.

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
-import { Card as C, Input } from "antd";
+import { Button, Card as C } from "antd";
+import { EllipsisOutlined, PlusOutlined, PicLeftOutlined } from "@ant-design/icons";
 import Card from "@components/Card";
-import Button from "@components/Buttons/Button";
-import { EllipsisOutlined, PlusOutlined, PicLeftOutlined, CloseOutlined } from "@ant-design/icons";
 import * as S from "./style";
 
-const { TextArea } = Input;
 const CardList = () => {
-  const [isWritingNewCard, setIsWritingNewCard] = useState(false);
-
-  const handleClickAddACard = () => {
-    setIsWritingNewCard(!isWritingNewCard);
-  };
-
   return (
     <S.Container>
       <C
@@ -24,25 +15,12 @@ const CardList = () => {
         bodyStyle={S.Body}
       >
         <Card text="card" />
-        {isWritingNewCard ? (
-          <S.CardInputContainer>
-            <C bodyStyle={S.CardInput}>
-              <TextArea placeholder="Enter a title for this card..." autoSize bordered={false} />
-            </C>
-            <S.AddCardButtonContainer>
-              <S.AddCardButtonWrapper>
-                <Button buttonColor="blue">Add card</Button>
-                <CloseOutlined style={S.CancleAddCardButton} onClick={handleClickAddACard} />
-              </S.AddCardButtonWrapper>
-              <Button icon={<EllipsisOutlined />} />
-            </S.AddCardButtonContainer>
-          </S.CardInputContainer>
-        ) : (
-          <S.ButtonWrapper onClick={handleClickAddACard}>
-            <Button icon={<PlusOutlined />}>Add a card</Button>
-            <Button icon={<PicLeftOutlined />} />
-          </S.ButtonWrapper>
-        )}
+        <S.ButtonWrapper>
+          <Button type="ghost" block icon={<PlusOutlined />} style={{ textAlign: "left", padding: 0 }}>
+            Add a card
+          </Button>
+          <Button type="ghost" block icon={<PicLeftOutlined />} style={S.TemplateButton}></Button>
+        </S.ButtonWrapper>
       </C>
     </S.Container>
   );
