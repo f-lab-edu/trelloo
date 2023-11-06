bb9536143b0f14a3453c2281ffabaeac991ff82a
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor Drawer to compound component, Add Drawer components (#9)"
    This reverts commit 4b9c9d676ea6dc126b48f7f64bb03fd03f539039.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,16 +15,4 @@ export const STORAGE_KEY = {
 
 export const SEARCH_PARAMS_KEY = {
   SEARCH: "search",
-  MENU: "menu",
-};
-
-export const DRAWER_MENU_SEARCH_PARAMS = {
-  ALL: "all",
-  CHANGE_BACKGROUND: "change_background",
-  PHOTOS: "photos",
-};
-
-export const DRAWER_CONTENT = {
-  CHANGE_BACKGROUND: "changeBackground",
-  PHOTOS: "photos",
 };
