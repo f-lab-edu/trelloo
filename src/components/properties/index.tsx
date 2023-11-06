8e46bf19882133cda8cafa0f26f39fcaef3453a7
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: SelectControl"
    This reverts commit 51888ed763bccda039eaca1ed46af9825b009ee0.

--- src/components/properties/index.tsx
+++ src/components/properties/index.tsx
@@ -4,7 +4,6 @@ import ToggleControl from "./controls/ToggleControl";
 import SliderControl from "./controls/SliderControl";
 import ImageControl from "./controls/ImageControl";
 import ColorControl from "./controls/ColorControl";
-import SelectControl from "./controls/SelectControl";
 
 function Properties() {}
 
@@ -14,6 +13,5 @@ Properties.ToggleControl = ToggleControl;
 Properties.SliderControl = SliderControl;
 Properties.ImageControl = ImageControl;
 Properties.ColorControl = ColorControl;
-Properties.SelectControl = SelectControl;
 
 export default Properties;
