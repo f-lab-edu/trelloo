01425f3f3c999ae1e687dc43171eab64852b007e
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "CHore: add clickoutside, drag and drop library (#11)"
    This reverts commit 1e4fe416bf36efffb93a44884ea50f0013378169.

--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -1,19 +1,10 @@
 import styled from "styled-components";
 
-export const DimmedBackground = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${({ theme }) => theme.color.dimmedBackground};
-`;
-
 export const Container = styled.div`
   // temporal style
   position: fixed;
   width: 300px;
   height: 300px;
-  background-color: white;
+  background: beige;
   transform: translate(25vw, 25vh);
 `;
