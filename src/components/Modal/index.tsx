d8b7637944ca319854c7febbdacbe6dcf1d8027a
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "CHore: add clickoutside, drag and drop library (#11)"
    This reverts commit 1e4fe416bf36efffb93a44884ea50f0013378169.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,9 +1,6 @@
-import { useRef } from "react";
 import { useSelector } from "react-redux";
 import { createPortal } from "react-dom";
 import { MODAL_TYPE } from "@utils/constants";
-import useOutsideClick from "@utils/useClickOutside";
-import useModal from "@utils/hooks/useModal";
 import { ModalState } from "@/store/slices/modal";
 import CardDetail from "@components/CardDetail";
 import CardEdit from "@components/CardEdit";
@@ -14,13 +11,11 @@ interface ModalComponent {
 }
 
 const Modal = () => {
-  const { closeModal } = useModal();
-  const clickOutsideRef = useRef<HTMLDivElement>(null);
   const { type, options, props } = useSelector(
     (state: { modal: ModalState }) => state.modal
   );
   const isModalOpened = type !== MODAL_TYPE.NONE ? true : false;
-  useOutsideClick(clickOutsideRef, () => closeModal());
+
   const modalComponent: ModalComponent = {
     [MODAL_TYPE.CARD_EDIT]: <CardEdit />,
     [MODAL_TYPE.CARD_DETAIL]: <CardDetail />,
@@ -29,11 +24,9 @@ const Modal = () => {
   return (
     <ModalPortal>
       {isModalOpened && (
-        <S.DimmedBackground>
-          <S.Container ref={clickOutsideRef}>
-            {modalComponent[type] && modalComponent[type]}
-          </S.Container>
-        </S.DimmedBackground>
+        <S.Container>
+          {modalComponent[type] && modalComponent[type]}
+        </S.Container>
       )}
     </ModalPortal>
   );
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,5 @@
 import { DefaultTheme } from "styled-components"; // ✨ 1
 
 export const theme: DefaultTheme = {
-  color: {
-    dimmedBackground: "#000000a3",
-    backgroundGray: "#F3F5F7", // modal background
-    pointGray: "#EAECF0", // list backgoround, button
-    hoverPointGray: "#E1E4E9",
-    textPointGray: "#5E6C83",
-    textDefaultGray: "#3C3E3F",
-  },
+  //
 };
