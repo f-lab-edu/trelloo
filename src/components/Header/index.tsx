6f32e5be00741e34a646c89c320f45c00e1e2c57
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor: seperate elements in BoardPage to components"
    This reverts commit 898a6adc714d9c3a8bf49826e91f1b144d351db1.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import { Layout } from "antd";
-
-const { Header: H } = Layout;
+import * as S from "./style";
 
 function Header() {
-  return (
-    <H style={{ position: "sticky", top: 0, zIndex: 1, width: "100%", backgroundColor: "white" }}>
-      <div>header</div>
-    </H>
-  );
+  return <S.Container>Header</S.Container>;
 }
 
 export default Header;
--- src/pages/BoardPage/index.tsx
+++ src/pages/BoardPage/index.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
-import { Breadcrumb, Layout, theme, Button } from "antd";
-import Slider from "@components/Slider";
-import Drawer from "@components/Drawer";
-import Header from "@components/Header";
-import Menu from "@components/Menu";
-import Board from "@components/Board";
+import { Breadcrumb, Layout, theme, Button, Drawer } from "antd";
 
-const { Content } = Layout;
+const { Header, Content, Sider } = Layout;
 
 const BoardPage: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
   const [open, setOpen] = useState(false);
 
   const showDrawer = () => {
@@ -26,15 +22,46 @@ const BoardPage: React.FC = () => {
     token: { colorBgContainer },
   } = theme.useToken();
 
+  const onClickCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Header />
+      <Header style={{ position: "sticky", top: 0, zIndex: 1, width: "100%", backgroundColor: "white" }}>
+        <div>header</div>
+      </Header>
       <Layout className="site-layout">
-        <Slider />
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+          style={{ backgroundColor: "white" }}
+        >
+          <button onClick={onClickCollapse}>collapse</button>
+        </Sider>
         <Content style={containerStyle}>
-          <Menu showDrawer={showDrawer} />
-          <Board boardName={"sdf"} />
-          <Drawer open={open} onClose={onClose} />
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <div>menu</div>
+            <div style={{ marginTop: 16 }}>
+              <Button type="primary" onClick={showDrawer}>
+                Open
+              </Button>
+            </div>
+          </Breadcrumb>
+          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>cards</div>
+          <div>
+            <Drawer
+              title="Basic Drawer"
+              placement="right"
+              closable={false}
+              onClose={onClose}
+              open={open}
+              getContainer={false}
+            >
+              <p>Some contents...</p>
+            </Drawer>
+          </div>
         </Content>
       </Layout>
     </Layout>
