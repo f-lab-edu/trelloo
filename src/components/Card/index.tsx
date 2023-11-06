edb106fc8add85a1d284199849e3f542c80b52f9
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Correct: resolve git conflict"
    This reverts commit 664d3a0dc0ae781aef14216c57de19f50fa0329c.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,3 @@
-import { Card as C } from "antd";
 import loadable from "@loadable/component";
 import useModal from "@/hooks/useModal";
 const CardDetail = loadable(() => import("@components/Modals/CardDetail"));
@@ -19,13 +18,7 @@ const Card = ({ text }: Props) => {
     });
   };
 
-  return (
-    <S.Container>
-      <C style={S.Card} onClick={handleClick} bodyStyle={S.Body}>
-        <p>{text}</p>
-      </C>
-    </S.Container>
-  );
+  return <S.Container onClick={handleClick}>{text && text}</S.Container>;
 };
 
 export default Card;
