02ab527b0c9a6d621f81f265347320940a72c441
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: Input common component"
    This reverts commit a039fd63814d45924fdfd4ad0dfe9ad325204430.

--- src/components/menus/FilterMenu/index.tsx
+++ src/components/menus/FilterMenu/index.tsx
@@ -1,26 +1,11 @@
 import React from "react";
-import Input from "@components/inputs/Input";
-import { useForm } from "react-hook-form";
 import * as S from "./style";
 
 function FilterMenu() {
-  const { handleSubmit, control } = useForm({
-    defaultValues: {
-      filterMenu: "",
-    },
-    mode: "onChange",
-  });
-
-  const onSubmit = (data: any) => {
-    // TODO: add onSubmit
-  };
-
   return (
     <div>
       <S.Title>Keyword</S.Title>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input control={control} placeHolder="Enter a keyword..." name="filterMenu" />
-      </form>
+      <S.Input placeholder="Enter a keyword..." />
       <S.Description>Search cards, members, labels, and more.</S.Description>
     </div>
   );
