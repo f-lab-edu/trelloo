fd05dde61bd7f044a7bc8a2a3877f2f1013c9083
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: EmptyBoard in ErrorBoundary"
    This reverts commit b77151ae048c67a14d8bd0062bb2dfc9430ee00e.

--- src/components/EmptyBoard/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-  padding-top: 200px;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-`;
--- src/pages/BoardPage/index.tsx
+++ src/pages/BoardPage/index.tsx
@@ -1,13 +1,11 @@
 import React, { useState, Suspense } from "react";
 import { Layout } from "antd";
-import { ErrorBoundary } from "react-error-boundary";
 import Sider from "@components/Sider";
 import Drawer from "@components/Drawer";
 import Header from "@components/Header";
 import Menu from "@components/Menu";
 import Board from "@components/Board";
 import BoardSkeleton from "@components/skeletons/BoardSkeleton";
-import EmptyBoard from "@components/EmptyBoard";
 import * as S from "./style";
 
 const { Content } = Layout;
@@ -30,11 +28,9 @@ const BoardPage: React.FC = () => {
         <Sider />
         <Content style={S.Content}>
           <Menu showDrawer={showDrawer} boardName={"boardName"} />
-          <ErrorBoundary FallbackComponent={EmptyBoard}>
-            <Suspense fallback={<BoardSkeleton />}>
-              <Board />
-            </Suspense>
-          </ErrorBoundary>
+          <Suspense fallback={<BoardSkeleton />}>
+            <Board />
+          </Suspense>
           <Drawer open={open} onClose={onClose} />
         </Content>
       </Layout>
