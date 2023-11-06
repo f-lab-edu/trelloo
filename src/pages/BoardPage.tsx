d3a9e51d369d4d2866574a28cef2348871455100
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: Header component"
    This reverts commit 4b45d32b5d27bbc5c44d39f2ca10c1953d36982c.

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,11 +1,9 @@
-import Header from "@components/Header";
 import Board from "@components/Board";
 import * as S from "./style";
 
 const BoardPage = () => {
   return (
     <S.Container>
-      <Header />
       <Board boardName="test board name" />
     </S.Container>
   );
