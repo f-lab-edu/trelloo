31b418a2a2f0f597114f6a4beb89443e0f205542
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: SliderControl, ColorControl, ImageControl (#19)"
    This reverts commit 6533241b5446950f3a9329306228d2c69b6cb050.

--- src/components/properties/controls/ColorControl/style.ts
+++
@@ -1,8 +0,0 @@
-import { Button as ShadCnButton } from "@/components/ui/button";
-import styled from "styled-components";
-
-export const Button = styled(ShadCnButton)`
-  width: 100px;
-  height: 25px;
-  background-color: ${({ color }) => color};
-`;
