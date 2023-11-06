10cf69ff5515bf1de7b43e1c2ad5439885adc92a
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Correct: rename fetchRequest parameters"
    This reverts commit 7cb1c9202eed031cdcfbe7386dd6e1054c6bfb0a.

--- src/interfaces/httpRequest.ts
+++ src/interfaces/httpRequest.ts
@@ -1,36 +1,36 @@
-export interface RequestParams<TQueryParams = Params, TParams = {}> {
+export interface RequestParams<TParams = Params, TData = {}> {
   path: string;
   method?: RequestMethod;
+  data?: TData;
   params?: TParams;
-  queryParams?: TQueryParams;
   isMock?: boolean;
 }
 
 export type Params = Record<string, string>;
 type RequestMethod = "get" | "post" | "put" | "delete";
 
-export interface RequestOptions<TParams> {
-  onSuccess?: (data: TParams) => void;
+export interface RequestOptions<TData> {
+  onSuccess?: (data: TData) => void;
   onError?: (error: Error) => void;
 }
 
-export interface HandleUseQueryParams<TQueryParams = Params, TParams = object> {
+export interface HandleUseQueryParams<TParams = Params, TData = object> {
   key: string;
   path: string;
-  queryParams?: TQueryParams;
-  options?: RequestOptions<TParams>;
+  params?: TParams;
+  options?: RequestOptions<TData>;
 }
 
 export interface HandleUseMutationParams<
-  TQueryParams = Params,
-  TParams = object,
+  TParams = Params,
+  TData = object,
   TRes = object
 > {
   key?: string;
   path: string;
   method: RequestMethod;
-  queryParams?: TQueryParams;
   params?: TParams;
+  data?: TData;
   options?: RequestOptions<TRes>;
   res?: TRes;
 }
