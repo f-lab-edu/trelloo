3d397b678d4a2991b32a913eadcbba23447a7243
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Chore: rename CardEdit to CardEditor"
    This reverts commit a784a1b5d8e8d48a0a378dd0bbb265b96b20a1ff.

---
+++ src/components/Modals/CardEdit/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+export interface Props {
+  text: string;
+}
+
+const CardEdit = ({ text }: Props) => {
+  return <S.Container>CardEdit</S.Container>;
+};
+
+export default CardEdit;
