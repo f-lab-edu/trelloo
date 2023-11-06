a935328f0852d0e649aa4d985bc372a38d615699
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor: seperate cardlist dropdown as a component"
    This reverts commit 703197d5d9d153c4c1dabe31d2b2b4882256e36e.

--- src/components/CardList/style.ts
+++ src/components/CardList/style.ts
@@ -34,3 +34,9 @@ export const ListOption = styled.div`
   left: 250px;
   z-index: 10;
 `;
+
+export const ListOptionTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 280px;
+`;
