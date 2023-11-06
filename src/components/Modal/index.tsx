b513bb296b98d68402c799764a017f5017fee9bb
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Fix: modalComponent props type setting any type as default"
    This reverts commit ad2cf53f7be7b5b23dfefbf63e5187409dc63c39.

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -6,9 +6,12 @@ import useModal from "@/hooks/useModal";
 import { ModalState } from "@/store/slices/modal";
 import * as S from "./style";
 
-interface ModalComponent<T = any> {
-  [x: string]: React.LazyExoticComponent<(props: T) => JSX.Element>;
+interface ModalComponent {
+  [x: string]: CardEdit | CardDetail;
 }
+type Props<T> = React.LazyExoticComponent<(props: T) => JSX.Element>;
+type CardEdit = Props<{ text: string }>;
+type CardDetail = Props<{ title: string }>;
 
 const Modal = () => {
   const { closeModal } = useModal();
@@ -23,6 +26,11 @@ const Modal = () => {
       () => import("@components/CardDetail")
     ),
   };
+
+  interface Modals {
+    [MODAL_TYPE.CARD_EDIT]: CardEdit;
+    [MODAL_TYPE.CARD_DETAIL]: CardDetail;
+  }
 
   const CurrentComponent = modalComponent[type];
 
