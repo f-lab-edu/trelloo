d1fd4cacecdcadefdca3be200cb29e47c76b3480
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: Layout of BoardPage"
    This reverts commit 8c3d135869b7cb9dec17192d57d027364a6c08d4.

--- src/pages/BoardPage/index.tsx
+++ src/pages/BoardPage/index.tsx
@@ -1,70 +1,13 @@
-import React, { useState } from "react";
-import { Breadcrumb, Layout, theme, Button, Drawer } from "antd";
+import Header from "@components/Header";
+import Board from "@components/Board";
+import * as S from "./style";
 
-const { Header, Content, Sider } = Layout;
-
-const BoardPage: React.FC = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const [open, setOpen] = useState(false);
-
-  const showDrawer = () => {
-    setOpen(true);
-  };
-
-  const onClose = () => {
-    setOpen(false);
-  };
-
-  const containerStyle: React.CSSProperties = {
-    position: "relative",
-  };
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
-
-  const onClickCollapse = () => {
-    setCollapsed(!collapsed);
-  };
-
+const BoardPage = () => {
   return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Header style={{ position: "sticky", top: 0, zIndex: 1, width: "100%", backgroundColor: "white" }}>
-        <div>header</div>
-      </Header>
-      <Layout className="site-layout">
-        <Sider
-          collapsible
-          collapsed={collapsed}
-          onCollapse={(value) => setCollapsed(value)}
-          style={{ backgroundColor: "white" }}
-        >
-          <button onClick={onClickCollapse}>collapse</button>
-        </Sider>
-        <Content style={containerStyle}>
-          <Breadcrumb style={{ margin: "16px 0" }}>
-            <div>menu</div>
-            <div style={{ marginTop: 16 }}>
-              <Button type="primary" onClick={showDrawer}>
-                Open
-              </Button>
-            </div>
-          </Breadcrumb>
-          <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>cards</div>
-          <div>
-            <Drawer
-              title="Basic Drawer"
-              placement="right"
-              closable={false}
-              onClose={onClose}
-              open={open}
-              getContainer={false}
-            >
-              <p>Some contents...</p>
-            </Drawer>
-          </div>
-        </Content>
-      </Layout>
-    </Layout>
+    <S.Container>
+      <Header />
+      <Board boardName="test board name" />
+    </S.Container>
   );
 };
 
