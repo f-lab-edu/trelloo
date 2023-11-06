8cc24b5c2f4f156a3ca53863b13b7d89645a3be5
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Correct: add key for mapping element"
    This reverts commit 8708fb8c9ec492238805d7fe4daa4bfe02872a3e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BoardPage from "./pages/BoardPage";
+import ModalsProvider from "@components/Modals/ModalsProvider";
 import Modals from "@components/Modals/Modals";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<BoardPage />} />
-      </Routes>
-      <Modals />
-    </BrowserRouter>
+    <ModalsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BoardPage />} />
+        </Routes>
+        <Modals />
+      </BrowserRouter>
+    </ModalsProvider>
   );
 }
 
