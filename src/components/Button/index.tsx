05f6e6508f4840038ac84f937e11c4a2c06d0275
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: disable add card button on loading"
    This reverts commit b6d6241dd89b4ec8915abb5b7bdfc517a4761dbf.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import * as S from "./style";
 
 interface Props {
-  isLoading?: boolean;
   Icon?: React.ReactNode;
   isIconBehindText?: boolean;
   type?: "button" | "submit" | "reset";
@@ -14,9 +13,9 @@ interface Props {
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-function Button({ Icon, isIconBehindText = false, type = "button", appearance, onClick, children, isLoading }: Props) {
+function Button({ Icon, isIconBehindText = false, type = "button", appearance, onClick, children }: Props) {
   return (
-    <S.Container type={type} style={appearance?.style} onClick={onClick} appearance={appearance} disabled={isLoading}>
+    <S.Container type={type} style={appearance?.style} onClick={onClick} appearance={appearance}>
       <S.IconWrapper>{!isIconBehindText && !!Icon && Icon}</S.IconWrapper>
       {children}
       <S.IconBehindWrapper>{isIconBehindText && Icon}</S.IconBehindWrapper>
