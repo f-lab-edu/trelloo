b7b1186922af92eb624e767f94d5b4c3b463550d
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor: use code splitting to render modal content"
    This reverts commit f0aace3ae75418974032757fe53beb8e54dc303f.

--- src/components/CardDetail/index.tsx
+++ src/components/CardDetail/index.tsx
@@ -1,12 +1,12 @@
 import useModal from "@utils/hooks/useModal";
 import * as S from "./style";
 
-const CardDetail = ({ title }: { title: string }) => {
+const CardDetail = () => {
   const { closeModal } = useModal();
 
   return (
     <S.Container>
-      {title}
+      CardDetail
       <S.CloseButton onClick={closeModal}>X</S.CloseButton>
     </S.Container>
   );
