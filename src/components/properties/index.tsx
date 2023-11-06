955495ffd10c002c1ea7f97256607f9a54c61de0
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Add: PropertyVertical, CoordControl"
    This reverts commit ae0588fa94dacf49cc830b16b5ce8a1d7d84a459.

--- src/components/properties/index.tsx
+++ src/components/properties/index.tsx
@@ -5,19 +5,15 @@ import SliderControl from "./controls/SliderControl";
 import ImageControl from "./controls/ImageControl";
 import ColorControl from "./controls/ColorControl";
 import SelectControl from "./controls/SelectControl";
-import CoordControl from "./controls/CoordControl";
-import PropertyVertical from "./PropertyVertical";
 
 function Properties() {}
 
 Properties.Group = PropertyGroup;
 Properties.Property = Property;
-Properties.PropertyVertical = PropertyVertical;
 Properties.ToggleControl = ToggleControl;
 Properties.SliderControl = SliderControl;
 Properties.ImageControl = ImageControl;
 Properties.ColorControl = ColorControl;
 Properties.SelectControl = SelectControl;
-Properties.CoordControl = CoordControl;
 
 export default Properties;
