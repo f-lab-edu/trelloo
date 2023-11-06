6822cb62c3319fd55928fcf5b38d438d11e5a996
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Chore: rename Modals folder to modals"
    This reverts commit 3186d6622d300c1a6329d0d24e43f992e3073cf4.

--- src/components/modals/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import loadable from "@loadable/component";
-
-const CardDetailModal = loadable(() => import("@components/modals/components/CardDetailModal"));
-const InviteToWorkspaceModal = loadable(() => import("@components/modals/components/InviteToWorkspaceModal"));
-
-export const modals = {
-  cardDetailModal: CardDetailModal,
-  inviteToWorkspaceModal: InviteToWorkspaceModal,
-};
