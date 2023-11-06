17c57d94ffbd11a4b003a7ef475f4d7055f2938b
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor: remove wrapping event handler props func"
    This reverts commit 5f010af280063af9cdd1d95c66b975ae1d182268.

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -59,12 +59,16 @@ const CardList = ({ data, onEditList, onDeleteList }: Props) => {
     deleteCardMutate(params);
   };
 
+  const handleDeleteListById = () => {
+    onDeleteList({ id: data.id });
+  };
+
   return (
     <S.Container>
       <AntdCard
         bordered={false}
         style={{ width: 300 }}
-        extra={<ListOptions onDeleteList={() => onDeleteList({ id: data.id })} />}
+        extra={<ListOptions onDeleteListById={handleDeleteListById} />}
         headStyle={S.Header}
         bodyStyle={S.Body}
       >
