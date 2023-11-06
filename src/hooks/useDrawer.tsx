7dedf6994a6b4a13f39178894b3c0c88e7c67d98
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Correct: rename MenuTitle (#14)"
    This reverts commit c576e86a040bd31d6ffb4023856e0a19e5c412f0.

--- src/hooks/useDrawer.tsx
+++ src/hooks/useDrawer.tsx
@@ -1,11 +1,8 @@
 import { DrawerDispatchContext } from "@components/providers/DrawerProvider";
 import { useContext } from "react";
-import useFunnel from "./useFunnel";
-import { SEARCH_PARAMS_KEY } from "@/constants";
 
 const useDrawer = () => {
   const drawerDispatch = useContext(DrawerDispatchContext);
-  const { initializeStep, goBack } = useFunnel();
 
   const openDrawer = () => {
     drawerDispatch?.open();
@@ -13,10 +10,8 @@ const useDrawer = () => {
 
   const closeDrawer = () => {
     drawerDispatch?.close();
-    initializeStep(SEARCH_PARAMS_KEY.MENU);
   };
-
-  return { openDrawer, closeDrawer, goBack };
+  return { openDrawer, closeDrawer };
 };
 
 export default useDrawer;
