f67ffaae061dd7027cab383dd6d7990882c50451
gygy <67543454+Gayun00@users.noreply.github.com>
2023-11-06 13:39:23 +0900

    Revert "Refactor: fetchRequest, queries"
    This reverts commit 5ce2242c5097b17dea13665f71fd9623d0adeb1a.

--- src/queries/Cards/index.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { request } from "@utils/httpRequest";
-
-export const useGetCardLists = () => {
-  return useQuery(["cardLists"], () => {
-    return request.get({ url: "/card/list" }).data;
-  });
-};
